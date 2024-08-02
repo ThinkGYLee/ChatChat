b4ffd4ac4a9c9d7613642c19c4701512d15b1226
ThinkGYLee <ntnpholicfp@gmail.com>
2024-08-03 04:20:19 +0900

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+import com.diffplug.gradle.spotless.SpotlessExtension
+
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.compose.compiler) apply false
+    alias(libs.plugins.spotless) apply false
+}
+subprojects {
+    plugins.apply("com.diffplug.spotless")
+
+    configure<SpotlessExtension> {
+        kotlin {
+            target("**/*.kt")
+            ktlint()
+            indentWithSpaces()
+            endWithNewline()
+        }
+        kotlinGradle {
+            target("**/*.gradle.kts")
+            ktlint()
+            indentWithSpaces()
+            endWithNewline()
+        }
+    }
+}
