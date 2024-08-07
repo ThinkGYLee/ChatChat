521df90548cf1e69f24592d9de72532bafe5b8f4
ThinkGYLee <ntnpholicfp@gmail.com>
2024-08-08 00:34:45 +0900

    - hilt 관련 클래스 어노테이션 세팅

---
+++ app/src/main/java/com/gyleedev/chatchat/ui/login/LoginScreen.kt
@@ -0,0 +1,11 @@
+package com.gyleedev.chatchat.ui.login
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun LoginScreen(
+    modifier: Modifier = Modifier
+) {
+
+}
