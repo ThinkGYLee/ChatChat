cc9d5470306833d460819d9f9b1f5519ce5157df
ThinkGYLee <ntnpholicfp@gmail.com>
2024-08-14 23:55:10 +0900

    - LoginScreen에 scaffold 추가

--- app/src/main/java/com/gyleedev/chatchat/ui/login/LoginScreen.kt
+++ app/src/main/java/com/gyleedev/chatchat/ui/login/LoginScreen.kt
@@ -1,13 +1,32 @@
 package com.gyleedev.chatchat.ui.login
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.consumeWindowInsets
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LoginScreen(
     modifier: Modifier = Modifier,
     viewModel: LoginViewModel = hiltViewModel()
 ) {
+    Scaffold(
+        topBar = { TopAppBar(title = { Text(text = "") }) },
+        modifier = modifier
+    ) { innerPadding ->
+        Column(modifier = Modifier
+            .padding(innerPadding)
+            .consumeWindowInsets(innerPadding)) {
+
+        }
+
+    }
 
 }
