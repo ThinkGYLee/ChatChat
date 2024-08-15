aea112f4e01ec365d8817fad8bf494f8d20dde48
ThinkGYLee <ntnpholicfp@gmail.com>
2024-08-15 22:29:45 +0900

    - LoginScreen에 title 추가

--- app/src/main/java/com/gyleedev/chatchat/ui/login/LoginScreen.kt
+++ app/src/main/java/com/gyleedev/chatchat/ui/login/LoginScreen.kt
@@ -1,7 +1,9 @@
 package com.gyleedev.chatchat.ui.login
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.consumeWindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
@@ -9,6 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.hilt.navigation.compose.hiltViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -21,12 +24,23 @@ fun LoginScreen(
         topBar = { TopAppBar(title = { Text(text = "") }) },
         modifier = modifier
     ) { innerPadding ->
-        Column(modifier = Modifier
-            .padding(innerPadding)
-            .consumeWindowInsets(innerPadding)) {
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .consumeWindowInsets(innerPadding)
+                .fillMaxSize()
+        ) {
+            
+            Row {
+                Text(text = "title")
+            }
 
         }
-
     }
+}
 
+@Preview
+@Composable
+fun LoginScreenPreview() {
+    LoginScreen()
 }
