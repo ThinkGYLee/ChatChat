e04e35ad083819a5e029a3cc2f580a2c139d227e
ThinkGYLee <ntnpholicfp@gmail.com>
2024-09-10 23:49:05 +0900

    - 로그인 뷰모델에 변수 추가

--- app/src/main/java/com/gyleedev/chatchat/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/gyleedev/chatchat/ui/login/LoginViewModel.kt
@@ -2,10 +2,18 @@ package com.gyleedev.chatchat.ui.login
 
 import com.gyleedev.chatchat.core.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import javax.inject.Inject
 
 @HiltViewModel
 class LoginViewModel @Inject constructor() : BaseViewModel() {
+
+    private val _idQuery = MutableStateFlow("")
+    val idQuery: StateFlow<String> = _idQuery
+
+    private val _passwordQuery = MutableStateFlow("")
+    val passwordQuery: StateFlow<String> = _passwordQuery
 
     fun logInButtonClick(
         id: String,
