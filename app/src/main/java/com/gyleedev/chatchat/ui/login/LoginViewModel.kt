b2d1fbfd49087a7e74d7a7e543bfaeea768255a7
ThinkGYLee <ntnpholicfp@gmail.com>
2024-09-12 23:45:50 +0900

    - 아이디 패스워드 변경 함수 추가

--- app/src/main/java/com/gyleedev/chatchat/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/gyleedev/chatchat/ui/login/LoginViewModel.kt
@@ -1,9 +1,11 @@
 package com.gyleedev.chatchat.ui.login
 
+import androidx.lifecycle.viewModelScope
 import com.gyleedev.chatchat.core.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
@@ -14,6 +16,18 @@ class LoginViewModel @Inject constructor() : BaseViewModel() {
 
     private val _passwordQuery = MutableStateFlow("")
     val passwordQuery: StateFlow<String> = _passwordQuery
+
+    fun editId(id: String) {
+        viewModelScope.launch {
+            _idQuery.emit(id)
+        }
+    }
+
+    fun editPassword(password: String) {
+        viewModelScope.launch {
+            _passwordQuery.emit(password)
+        }
+    }
 
     fun logInButtonClick(
         id: String,
