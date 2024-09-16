7fd1bfce54b224cb13d419bc5dff6a9af1bf9755
ThinkGYLee <ntnpholicfp@gmail.com>
2024-09-17 04:57:06 +0900

    - LoginViewModel에 validation 함수 추가

--- app/src/main/java/com/gyleedev/chatchat/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/gyleedev/chatchat/ui/login/LoginViewModel.kt
@@ -22,15 +22,29 @@ class LoginViewModel @Inject constructor() : BaseViewModel() {
     private val _passwordIsAvailable = MutableStateFlow(false)
     val passwordIsAvailable: StateFlow<Boolean> = _passwordIsAvailable
 
+    private val _logInIsAvailable = MutableStateFlow(false)
+    val logInIsAvailable: StateFlow<Boolean> = _logInIsAvailable
+
     fun editId(id: String) {
         viewModelScope.launch {
             _idQuery.emit(id)
+            checkSignInAvailable()
         }
     }
 
     fun editPassword(password: String) {
         viewModelScope.launch {
             _passwordQuery.emit(password)
+            checkSignInAvailable()
+        }
+    }
+
+    private fun checkSignInAvailable() {
+        viewModelScope.launch {
+            val pattern = android.util.Patterns.EMAIL_ADDRESS
+            _idIsAvailable.emit(pattern.matcher(_idQuery.value).matches())
+            _passwordIsAvailable.emit(_passwordQuery.value.length >= 8)
+            _logInIsAvailable.emit(_idIsAvailable.value && _passwordIsAvailable.value)
         }
     }
 
