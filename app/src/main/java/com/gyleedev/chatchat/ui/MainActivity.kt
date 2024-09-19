9ee94215c6a2ad2933324ab0af51160c903dfe9b
ThinkGYLee <ntnpholicfp@gmail.com>
2024-09-19 23:50:14 +0900

    - signin 관련 항목 액티비티에 추가

--- app/src/main/java/com/gyleedev/chatchat/ui/MainActivity.kt
+++ app/src/main/java/com/gyleedev/chatchat/ui/MainActivity.kt
@@ -1,21 +1,59 @@
 package com.gyleedev.chatchat.ui
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 import com.gyleedev.chatchat.ui.theme.ChatChatTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        auth = Firebase.auth
         enableEdgeToEdge()
         setContent {
             ChatChatTheme {
                 ChatChatScreen()
             }
         }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val currentUser = auth.currentUser
+        /*if (currentUser != null) {
+            //reload()
+        }*/
+    }
+
+    fun aa(email: String, password: String) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "createUserWithEmail:success")
+                    val user = auth.currentUser
+                    //updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                    Toast.makeText(
+                        baseContext,
+                        "Authentication failed.",
+                        Toast.LENGTH_SHORT,
+                    ).show()
+                    //updateUI(null)
+                }
+            }
     }
 }
