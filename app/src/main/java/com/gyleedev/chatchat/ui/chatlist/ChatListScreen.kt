48b3b434c92fe8aa768a0d0f5c52550618e15f7c
ThinkGYLee <ntnpholicfp@gmail.com>
2024-09-30 23:30:37 +0900

    - chatList 관련 파일 추가

---
+++ app/src/main/java/com/gyleedev/chatchat/ui/chatlist/ChatListScreen.kt
@@ -0,0 +1,53 @@
+package com.gyleedev.chatchat.ui.chatlist
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.consumeWindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ChatListScreen(
+    modifier: Modifier = Modifier,
+    viewModel: ChatListViewModel = hiltViewModel()
+) {
+    LaunchedEffect(Unit) {
+        viewModel.fetchState.collect {
+        }
+    }
+
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "Title")
+                }
+            )
+        },
+        modifier = modifier
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .consumeWindowInsets(innerPadding)
+                .fillMaxSize()
+        ) {
+            Row {
+                Text(text = "title")
+            }
+            Row {
+            }
+            Row {
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/gyleedev/chatchat/ui/chatlist/ChatListViewModel.kt
@@ -0,0 +1,8 @@
+package com.gyleedev.chatchat.ui.chatlist
+
+import com.gyleedev.chatchat.core.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class ChatListViewModel @Inject constructor() : BaseViewModel()
