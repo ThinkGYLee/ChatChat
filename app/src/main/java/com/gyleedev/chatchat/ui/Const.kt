37a8a1da1731e0f6aba57a987282d3ea582189b7
ThinkGYLee <ntnpholicfp@gmail.com>
2024-10-09 05:43:52 +0900

    - 사용안하는 const 제거

--- app/src/main/java/com/gyleedev/chatchat/ui/Const.kt
+++ app/src/main/java/com/gyleedev/chatchat/ui/Const.kt
@@ -1,8 +1,7 @@
 package com.gyleedev.chatchat.ui
 
-const val HOME = "CALENDAR"
 const val LOGIN = "LOGIN"
 const val SIGNIN = "SIGNIN"
-const val FAVORITE = "FAVORITE"
 const val FRIENDLIST = "FRIENDLIST"
 const val CHATLIST = "CHATLIST"
+const val SETTING = "SETTING"
