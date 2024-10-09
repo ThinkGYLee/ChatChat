f9805433047f2fa29166dc4a305967e060f0670a
ThinkGYLee <ntnpholicfp@gmail.com>
2024-10-10 03:11:30 +0900

    - 회원가입 관련 useCase 추가

---
+++ app/src/main/java/com/gyleedev/chatchat/domain/usecase/SignInDatabaseUseCase.kt
@@ -0,0 +1,19 @@
+package com.gyleedev.chatchat.domain.usecase
+
+import com.gyleedev.chatchat.data.repository.UserRepository
+import com.gyleedev.chatchat.domain.SignInResult
+import com.gyleedev.chatchat.domain.UserData
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+class SignInDatabaseUseCase @Inject constructor(
+    private val repository: UserRepository
+) {
+    suspend operator fun invoke(userData: UserData): Flow<SignInResult> {
+        return withContext(Dispatchers.IO) {
+            repository.writeUserToRealtimeDatabase(userData)
+        }
+    }
+}
