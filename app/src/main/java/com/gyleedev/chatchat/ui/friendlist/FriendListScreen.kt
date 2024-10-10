eda611fcf3cd25aac20e2d42936f967e59b69833
ThinkGYLee <ntnpholicfp@gmail.com>
2024-10-11 03:45:37 +0900

    - MyUserData Composable 구현

--- app/src/main/java/com/gyleedev/chatchat/ui/friendlist/FriendListScreen.kt
+++ app/src/main/java/com/gyleedev/chatchat/ui/friendlist/FriendListScreen.kt
@@ -1,10 +1,13 @@
 package com.gyleedev.chatchat.ui.friendlist
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
@@ -18,9 +21,14 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.gyleedev.chatchat.R
+import com.gyleedev.chatchat.domain.UserData
+import com.skydoves.landscapist.glide.GlideImage
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -32,6 +40,8 @@ fun FriendListScreen(
         viewModel.fetchState.collect {
         }
     }
+
+    val myUserData = viewModel.myUserData.collectAsStateWithLifecycle()
 
     Scaffold(
         topBar = {
@@ -55,6 +65,11 @@ fun FriendListScreen(
                 .fillMaxSize()
                 .padding(horizontal = 20.dp)
         ) {
+
+            if (myUserData.value != null) {
+                MyUserData(onClick = {}, userData = myUserData.value!!)
+            }
+
             Row {
                 Text(
                     text = "친구",
@@ -80,6 +95,28 @@ fun FriendListScreen(
             Row(modifier = Modifier.padding(vertical = 12.dp)) {
                 Text(text = "아빠")
             }
+        }
+    }
+}
+
+
+@Composable
+fun MyUserData(onClick: () -> Unit, userData: UserData, modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(vertical = 8.dp)
+            .clickable { onClick() },
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        GlideImage(imageModel = { userData.picture.ifBlank { R.drawable.icons8__ } })
+        Spacer(modifier = Modifier.width(20.dp))
+        Column(verticalArrangement = Arrangement.Center) {
+            Text(text = userData.name)
+            if (userData.status.isNotBlank()) Text(
+                text = userData.status,
+                style = MaterialTheme.typography.labelMedium
+            )
         }
     }
 }
