84cca19964c4c6a0e83f6546b34839fc80122ce3
ThinkGYLee <ntnpholicfp@gmail.com>
2024-10-11 03:35:13 +0900

    - UserData에 기본값 추가  - 검색시 crush나는 문제 해결

--- app/src/main/java/com/gyleedev/chatchat/domain/UserData.kt
+++ app/src/main/java/com/gyleedev/chatchat/domain/UserData.kt
@@ -2,10 +2,11 @@ package com.gyleedev.chatchat.domain
 
 import com.google.gson.annotations.SerializedName
 
+//기본값 설정 안해주면 crash남
 data class UserData(
-    @SerializedName("email") val email: String,
-    @SerializedName("name") val name: String,
-    @SerializedName("uid") val uid: String,
-    @SerializedName("picture") val picture: String,
-    @SerializedName("status") val status: String
+    @SerializedName("email") val email: String = "",
+    @SerializedName("name") val name: String = "",
+    @SerializedName("uid") val uid: String = "",
+    @SerializedName("picture") val picture: String = "",
+    @SerializedName("status") val status: String = ""
 )
