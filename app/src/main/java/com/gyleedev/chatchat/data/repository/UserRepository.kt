d06d7979f0ca87ad02b3ed48a614641af4e4d302
ThinkGYLee <ntnpholicfp@gmail.com>
2024-10-11 03:41:41 +0900

    - UserData 변경에 따른 repository 함수의 변경 - getMyUserInformation 함수 추가

--- app/src/main/java/com/gyleedev/chatchat/data/repository/UserRepository.kt
+++ app/src/main/java/com/gyleedev/chatchat/data/repository/UserRepository.kt
@@ -6,6 +6,7 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
 import com.gyleedev.chatchat.data.database.UserDao
 import com.gyleedev.chatchat.data.database.toEntity
 import com.gyleedev.chatchat.data.database.toModel
@@ -24,12 +25,14 @@ interface UserRepository {
     fun searchUser(email: String)
     fun fetchUserExists(): Boolean
     suspend fun writeUserToRealtimeDatabase(user: UserData): Flow<SignInResult>
+    suspend fun getMyUserInformation(): Flow<UserData?>
 }
 
 class UserRepositoryImpl @Inject constructor(
     private val userDao: UserDao,
-    private val firebase: Firebase,
-    private val auth: FirebaseAuth
+    firebase: Firebase,
+    private val auth: FirebaseAuth,
+    private val firebaseStorage: FirebaseStorage
 ) : UserRepository {
     val database =
         firebase.database("https://chat-a332d-default-rtdb.asia-southeast1.firebasedatabase.app/")
@@ -40,7 +43,15 @@ class UserRepositoryImpl @Inject constructor(
 
     override suspend fun signInUser(id: String, password: String): Flow<UserData?> = callbackFlow {
         auth.createUserWithEmailAndPassword(id, password).addOnSuccessListener { task ->
-            trySend(UserData(email = id, name = "Anonymous User", uid = task.user!!.uid, picture = " ", status = " "))
+            trySend(
+                UserData(
+                    email = id,
+                    name = "Anonymous User",
+                    uid = task.user!!.uid,
+                    picture = " ",
+                    status = " "
+                )
+            )
         }.addOnFailureListener {
             trySend(null)
         }
@@ -69,7 +80,7 @@ class UserRepositoryImpl @Inject constructor(
             auth.signInWithEmailAndPassword(id, password).addOnSuccessListener { task ->
                 trySend(LogInResult.Success)
             }.addOnFailureListener { task ->
-                val message = if(task.message!= null) task.message else "unknown failure"
+                val message = if (task.message != null) task.message else "unknown failure"
                 trySend(LogInResult.Failure(message!!))
             }
             awaitClose()
@@ -96,5 +107,23 @@ class UserRepositoryImpl @Inject constructor(
 
     override fun fetchUserExists(): Boolean {
         return auth.currentUser != null
+    }
+
+    override suspend fun getMyUserInformation(): Flow<UserData?> = callbackFlow {
+        val query =
+            database.reference.child("users").orderByChild("email").equalTo(auth.currentUser?.email)
+        query.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                for (ds in snapshot.getChildren()) {
+                    val snap = ds.getValue(UserData::class.java)
+                    trySend(snap)
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                trySend(null)
+            }
+        })
+        awaitClose()
     }
 }
