e8c3deae2fc3ad85f178669ddd5788ee91b83990
ThinkGYLee <ntnpholicfp@gmail.com>
2024-10-11 03:35:44 +0900

    - FirebaseModule에 FirebaseStorage 추가

--- app/src/main/java/com/gyleedev/chatchat/util/FirebaseModule.kt
+++ app/src/main/java/com/gyleedev/chatchat/util/FirebaseModule.kt
@@ -2,6 +2,7 @@ package com.gyleedev.chatchat.util
 
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -18,4 +19,8 @@ object FirebaseModule {
     @Provides
     @Singleton
     fun provideFirebase(): Firebase = Firebase
+
+    @Provides
+    @Singleton
+    fun provideFirebaseStorage(): FirebaseStorage = FirebaseStorage.getInstance()
 }
