3a07cbaf37b63d880af0f3305a474b82aa24183d
ThinkGYLee <ntnpholicfp@gmail.com>
2024-10-12 06:03:35 +0900

    - SettingScreen 수정

--- app/src/main/java/com/gyleedev/chatchat/ui/setting/SettingScreen.kt
+++ app/src/main/java/com/gyleedev/chatchat/ui/setting/SettingScreen.kt
@@ -1,45 +1,10 @@
 package com.gyleedev.chatchat.ui.setting
 
-import android.os.Build
-import androidx.annotation.RequiresApi
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.consumeWindowInsets
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
 
-@RequiresApi(Build.VERSION_CODES.O)
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SettingScreen(
-    onSignInClicked: () -> Unit,
-    modifier: Modifier = Modifier,
-    viewModel: SettingViewModel = hiltViewModel()
+    modifier: Modifier = Modifier
 ) {
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = {
-
-                }
-            )
-        },
-        modifier = modifier
-    ) { innerPadding ->
-        Column(
-            modifier = Modifier
-                .padding(innerPadding)
-                .consumeWindowInsets(innerPadding)
-                .fillMaxSize()
-                .padding(horizontal = 16.dp)
-        ) {
-        }
-    }
 }
