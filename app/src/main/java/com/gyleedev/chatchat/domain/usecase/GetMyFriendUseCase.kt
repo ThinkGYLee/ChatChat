422b5cd7e813dd25029cc396c9ed5b62713d3d7e
ThinkGYLee <ntnpholicfp@gmail.com>
2024-10-12 22:50:23 +0900

    - getyMyFriendUseCase 추가

---
+++ app/src/main/java/com/gyleedev/chatchat/domain/usecase/GetMyFriendUseCase.kt
@@ -0,0 +1,18 @@
+package com.gyleedev.chatchat.domain.usecase
+
+import com.gyleedev.chatchat.data.repository.UserRepository
+import com.gyleedev.chatchat.domain.UserData
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+class GetMyFriendUseCase @Inject constructor(
+    private val repository: UserRepository
+) {
+    suspend operator fun invoke(): Flow<List<UserData?>?> {
+        return withContext(Dispatchers.IO) {
+            repository.getMyFriendList()
+        }
+    }
+}
