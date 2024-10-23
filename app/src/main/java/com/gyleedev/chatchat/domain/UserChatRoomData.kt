b3a2dc70706d713500b40634b40977c100fe9bf0
ThinkGYLee <ntnpholicfp@gmail.com>
2024-10-23 21:43:22 +0900

    - UserChatRoomData의 id를 rid로 변경

--- app/src/main/java/com/gyleedev/chatchat/domain/UserChatRoomData.kt
+++ app/src/main/java/com/gyleedev/chatchat/domain/UserChatRoomData.kt
@@ -4,8 +4,8 @@ import com.google.gson.annotations.SerializedName
 
 // 기본값 설정 안해주면 crash남
 data class UserChatRoomData(
-    @SerializedName("id") val uid: String = "",
-    @SerializedName("receiver") val receiver: String = "",
+    @SerializedName("rid") val rid: String = "",
+    @SerializedName("receiver") val receiver: String = ""
 )
-//챗룸정보 테이블 -> 챗룸id를 키값으로
-//유저별 챗룸 정보 테이블 -> 유저 아이디를 키값으로 사용 내용에 상대Uid, 챗방Uid 저장
+// 챗룸정보 테이블 -> 챗룸id를 키값으로
+// 유저별 챗룸 정보 테이블 -> 유저 아이디를 키값으로 사용 내용에 상대Uid, 챗방Uid 저장
