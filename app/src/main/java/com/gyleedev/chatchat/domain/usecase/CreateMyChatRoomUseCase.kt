73d968875e6ccbd11da5d5c6bb4934c590fc69fd
ThinkGYLee <ntnpholicfp@gmail.com>
2024-10-27 06:48:00 +0900

    - CreateMyChatRoom 유스케이스 추가

---
+++ app/src/main/java/com/gyleedev/chatchat/domain/usecase/CreateMyChatRoomUseCase.kt
@@ -0,0 +1,20 @@
+package com.gyleedev.chatchat.domain.usecase
+
+import com.gyleedev.chatchat.data.repository.UserRepository
+import com.gyleedev.chatchat.domain.ChatRoomData
+import com.gyleedev.chatchat.domain.FriendData
+import com.gyleedev.chatchat.domain.UserChatRoomData
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+class CreateMyChatRoomUseCase @Inject constructor(
+    private val repository: UserRepository
+) {
+    suspend operator fun invoke(friend: FriendData, chatRoomData: ChatRoomData): Flow<UserChatRoomData?> {
+        return withContext(Dispatchers.IO) {
+            repository.createMyUserChatRoom(friend, chatRoomData)
+        }
+    }
+}
