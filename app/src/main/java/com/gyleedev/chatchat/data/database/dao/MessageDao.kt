83cf8f2bc7755b905d649733707d68ab6d38875a
ThinkGYLee <ntnpholicfp@gmail.com>
2024-10-28 23:47:27 +0900

    - getUsersWithPaging 추가

--- app/src/main/java/com/gyleedev/chatchat/data/database/dao/MessageDao.kt
+++ app/src/main/java/com/gyleedev/chatchat/data/database/dao/MessageDao.kt
@@ -1,11 +1,16 @@
 package com.gyleedev.chatchat.data.database.dao
 
+import androidx.paging.PagingSource
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 import com.gyleedev.chatchat.data.database.entity.MessageEntity
 
 @Dao
 interface MessageDao {
     @Insert
     fun insertChatRoom(message: MessageEntity): Long
+
+    @Query("SELECT * FROM message WHERE rid = :id")
+    fun getUsersWithPaging(id: String): PagingSource<Int, MessageEntity>
 }
