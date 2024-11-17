9244fddb8fdeb2f4c4ec0f7160d4be4475cef8b0
ThinkGYLee <ntnpholicfp@gmail.com>
2024-11-17 16:29:07 +0900

    - remote에 챗룸이 존재하는지 확인하는 유스케이스 추가

--- app/src/main/java/com/gyleedev/chatchat/domain/usecase/CheckChatRoomExistsUseCase.kt
+++ app/src/main/java/com/gyleedev/chatchat/domain/usecase/CheckChatRoomExistsUseCase.kt
@@ -3,16 +3,21 @@ package com.gyleedev.chatchat.domain.usecase
 import com.gyleedev.chatchat.data.repository.UserRepository
 import com.gyleedev.chatchat.domain.FriendData
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.firstOrNull
+import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class CheckChatRoomExistsUseCase @Inject constructor(
     private val repository: UserRepository
 ) {
-    suspend operator fun invoke(friend: FriendData): Flow<Boolean> {
+    suspend operator fun invoke(friend: FriendData): Boolean {
         return withContext(Dispatchers.IO) {
-            repository.checkChatRoomExistsInRemote(friend)
+            var result: Boolean
+            runBlocking {
+                result = repository.checkChatRoomExistsInRemote(friend).firstOrNull() ?: false
+            }
+            result
         }
     }
 }
