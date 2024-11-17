f07c6ba1afbce4ace5011304ea159dcadcc520cd
ThinkGYLee <ntnpholicfp@gmail.com>
2024-11-17 16:32:28 +0900

    - 내 챗룸과 친구 챗룸을 remote에 insert하는 유스케이스의 리턴타입을 flow에서 unit으로 변경

--- app/src/main/java/com/gyleedev/chatchat/domain/usecase/CreateFriendChatRoomUseCase.kt
+++ app/src/main/java/com/gyleedev/chatchat/domain/usecase/CreateFriendChatRoomUseCase.kt
@@ -3,9 +3,7 @@ package com.gyleedev.chatchat.domain.usecase
 import com.gyleedev.chatchat.data.repository.UserRepository
 import com.gyleedev.chatchat.domain.ChatRoomData
 import com.gyleedev.chatchat.domain.FriendData
-import com.gyleedev.chatchat.domain.UserChatRoomData
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
@@ -15,7 +13,7 @@ class CreateFriendChatRoomUseCase @Inject constructor(
     suspend operator fun invoke(
         friend: FriendData,
         chatRoomData: ChatRoomData
-    ): Flow<UserChatRoomData?> {
+    ) {
         return withContext(Dispatchers.IO) {
             repository.createFriendUserChatRoom(friend, chatRoomData)
         }
--- app/src/main/java/com/gyleedev/chatchat/domain/usecase/CreateMyChatRoomUseCase.kt
+++ app/src/main/java/com/gyleedev/chatchat/domain/usecase/CreateMyChatRoomUseCase.kt
@@ -3,16 +3,14 @@ package com.gyleedev.chatchat.domain.usecase
 import com.gyleedev.chatchat.data.repository.UserRepository
 import com.gyleedev.chatchat.domain.ChatRoomData
 import com.gyleedev.chatchat.domain.FriendData
-import com.gyleedev.chatchat.domain.UserChatRoomData
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class CreateMyChatRoomUseCase @Inject constructor(
     private val repository: UserRepository
 ) {
-    suspend operator fun invoke(friend: FriendData, chatRoomData: ChatRoomData): Flow<UserChatRoomData?> {
+    suspend operator fun invoke(friend: FriendData, chatRoomData: ChatRoomData) {
         return withContext(Dispatchers.IO) {
             repository.createMyUserChatRoom(friend, chatRoomData)
         }
