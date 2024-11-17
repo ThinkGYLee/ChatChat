9aa963a6baff389a2e1bca0147d3cbb9407975b8
ThinkGYLee <ntnpholicfp@gmail.com>
2024-11-17 16:31:40 +0900

    - room에 만들어진 채팅창 정보 입력하는 유스케이스 내용변경

--- app/src/main/java/com/gyleedev/chatchat/domain/usecase/InsertChatRoomToLocalUseCase.kt
+++ app/src/main/java/com/gyleedev/chatchat/domain/usecase/InsertChatRoomToLocalUseCase.kt
@@ -3,7 +3,6 @@ package com.gyleedev.chatchat.domain.usecase
 import com.gyleedev.chatchat.data.repository.UserRepository
 import com.gyleedev.chatchat.domain.ChatRoomData
 import com.gyleedev.chatchat.domain.FriendData
-import com.gyleedev.chatchat.domain.UserData
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
