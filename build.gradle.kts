4efd9a47593fdf36e5eb77e834d669ae27d727fe
ThinkGYLee <ntnpholicfp@gmail.com>
2024-11-29 02:13:49 +0900

    - crashlytics 적용

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,6 +16,7 @@ plugins {
     alias(libs.plugins.firebase) apply false
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.crashlytics) apply false
 }
 
 subprojects {
