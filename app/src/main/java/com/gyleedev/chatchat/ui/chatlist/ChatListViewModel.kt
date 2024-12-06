c3e92044e0f19c08250c34f6e3718314fc72da6a
ThinkGYLee <ntnpholicfp@gmail.com>
2024-12-06 23:55:54 +0900

    - ChatListViewModel에 repository 인젝트

--- app/src/main/java/com/gyleedev/chatchat/ui/chatlist/ChatListViewModel.kt
+++ app/src/main/java/com/gyleedev/chatchat/ui/chatlist/ChatListViewModel.kt
@@ -1,8 +1,13 @@
 package com.gyleedev.chatchat.ui.chatlist
 
 import com.gyleedev.chatchat.core.BaseViewModel
+import com.gyleedev.chatchat.data.repository.UserRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class ChatListViewModel @Inject constructor() : BaseViewModel()
+class ChatListViewModel @Inject constructor(
+    private val repository: UserRepository
+) : BaseViewModel() {
+
+}
