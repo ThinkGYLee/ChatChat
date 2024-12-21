71605fc3d28c84ce3c0f8331d2e3e26e729e3e86
ThinkGYLee <ntnpholicfp@gmail.com>
2024-12-22 05:40:17 +0900

    - ChatListViewModel 구현

--- app/src/main/java/com/gyleedev/chatchat/ui/chatlist/ChatListViewModel.kt
+++ app/src/main/java/com/gyleedev/chatchat/ui/chatlist/ChatListViewModel.kt
@@ -1,17 +1,46 @@
 package com.gyleedev.chatchat.ui.chatlist
 
 import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
 import androidx.paging.cachedIn
+import androidx.paging.map
 import com.gyleedev.chatchat.core.BaseViewModel
-import com.gyleedev.chatchat.data.repository.UserRepository
+import com.gyleedev.chatchat.domain.ChatRoomDataWithFriendAndMessage
+import com.gyleedev.chatchat.domain.MessageData
+import com.gyleedev.chatchat.domain.MessageSendState
+import com.gyleedev.chatchat.domain.usecase.GetChatRoomListUseCase
+import com.gyleedev.chatchat.domain.usecase.GetLastMessageUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class ChatListViewModel @Inject constructor(
-    private val repository: UserRepository
+    getChatRoomListUseCase: GetChatRoomListUseCase,
+    getLastMessageUseCase: GetLastMessageUseCase
 ) : BaseViewModel() {
 
-    val list = repository.getChatRoomListFromLocal().cachedIn(viewModelScope)
+    private val _chatRoomList =
+        MutableStateFlow<PagingData<ChatRoomDataWithFriendAndMessage>>(PagingData.empty())
+    val chatRoomList: StateFlow<PagingData<ChatRoomDataWithFriendAndMessage>> = _chatRoomList
 
+    init {
+        viewModelScope.launch {
+            getChatRoomListUseCase().cachedIn(viewModelScope).collectLatest { chatRoomAndFriend ->
+                _chatRoomList.emit(
+                    chatRoomAndFriend.map {
+                        ChatRoomDataWithFriendAndMessage(
+                            chatRoomLocalData = it.chatRoomLocalData,
+                            friendData = it.friendData,
+                            lastMessageData = getLastMessageUseCase(it.chatRoomLocalData)
+                                ?: MessageData("", "", "", 0L, MessageSendState.COMPLETE)
+                        )
+                    }
+                )
+            }
+        }
+    }
 }
