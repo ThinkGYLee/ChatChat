a74d281651b2f16e816d86f2307e90a5fecf8d7a
ThinkGYLee <ntnpholicfp@gmail.com>
2024-12-28 05:24:29 +0900

    - SendMessageUseCase 에 네트워크 값을 체크해서 인서트하고 업데이트하는 로직 추가

--- app/src/main/java/com/gyleedev/chatchat/domain/usecase/SendMessageUseCase.kt
+++ app/src/main/java/com/gyleedev/chatchat/domain/usecase/SendMessageUseCase.kt
@@ -2,6 +2,7 @@ package com.gyleedev.chatchat.domain.usecase
 
 import com.gyleedev.chatchat.data.repository.MessageRepository
 import com.gyleedev.chatchat.domain.MessageData
+import com.gyleedev.chatchat.domain.MessageSendState
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.withContext
@@ -10,13 +11,21 @@ import javax.inject.Inject
 class SendMessageUseCase @Inject constructor(
     private val repository: MessageRepository
 ) {
-    suspend operator fun invoke(messageData: MessageData, rid: Long) {
-        return withContext(Dispatchers.IO) {
+    suspend operator fun invoke(messageData: MessageData, rid: Long, networkState: Boolean) {
+        withContext(Dispatchers.IO) {
             val messageId = repository.insertMessageToLocal(messageData, rid)
-            if (messageId != null) {
-                val request = repository.insertMessageToRemote(messageData).first()
-                val message = messageData.copy(messageSendState = request)
-
+            if (networkState) {
+                val request = try {
+                    repository.insertMessageToRemote(messageData).first()
+                } catch (e: Throwable) {
+                    println(e)
+                }
+                if (request is MessageSendState) {
+                    val message = messageData.copy(messageSendState = request)
+                    repository.updateMessageState(messageId, rid, message)
+                }
+            } else {
+                val message = messageData.copy(messageSendState = MessageSendState.FAIL)
                 repository.updateMessageState(messageId, rid, message)
             }
         }
