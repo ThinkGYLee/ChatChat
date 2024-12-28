f438d2674c8a729fde69664f2f600b7f5219791a
ThinkGYLee <ntnpholicfp@gmail.com>
2024-12-29 06:46:21 +0900

    - SendMessageToRemoteUseCase 추가

--- app/src/main/java/com/gyleedev/chatchat/domain/usecase/SendMessageToRemoteUseCase.kt
+++ app/src/main/java/com/gyleedev/chatchat/domain/usecase/SendMessageToRemoteUseCase.kt
@@ -2,13 +2,11 @@ package com.gyleedev.chatchat.domain.usecase
 
 import com.gyleedev.chatchat.data.repository.MessageRepository
 import com.gyleedev.chatchat.domain.MessageData
-import kotlinx.coroutines.flow.first
 import javax.inject.Inject
 
-class ResendMessageUseCase @Inject constructor(
+class SendMessageToRemoteUseCase @Inject constructor(
     private val repository: MessageRepository
 ) {
-    suspend operator fun invoke(messageData: MessageData, rid: Long) {
-        repository.insertMessageToRemote(messageData).first()
-    }
+    operator fun invoke(messageData: MessageData) =
+        repository.insertMessageToRemote(messageData)
 }
