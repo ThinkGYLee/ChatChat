6ed82f72a426a332b04421c027c4872cf96b8ffa
ThinkGYLee <ntnpholicfp@gmail.com>
2024-12-29 07:01:34 +0900

    - DeleteMessageFromLocalUseCase 추가

---
+++ app/src/main/java/com/gyleedev/chatchat/domain/usecase/DeleteMessageFromLocalUseCase.kt
@@ -0,0 +1,12 @@
+package com.gyleedev.chatchat.domain.usecase
+
+import com.gyleedev.chatchat.data.repository.MessageRepository
+import com.gyleedev.chatchat.domain.MessageData
+import javax.inject.Inject
+
+class DeleteMessageFromLocalUseCase @Inject constructor(
+    private val repository: MessageRepository
+) {
+    suspend operator fun invoke(messageId: Long) =
+        repository.deleteMessage(messageId)
+}
