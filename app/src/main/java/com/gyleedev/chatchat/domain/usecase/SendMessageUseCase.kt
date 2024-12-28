0e06790121c349b780a2639254d9c5cde5844c1c
ThinkGYLee <ntnpholicfp@gmail.com>
2024-12-29 06:48:30 +0900

    - SendMessageUseCase 의 repository 의존성을 각 유스케이스로 분리

--- app/src/main/java/com/gyleedev/chatchat/domain/usecase/SendMessageUseCase.kt
+++ app/src/main/java/com/gyleedev/chatchat/domain/usecase/SendMessageUseCase.kt
@@ -1,6 +1,5 @@
 package com.gyleedev.chatchat.domain.usecase
 
-import com.gyleedev.chatchat.data.repository.MessageRepository
 import com.gyleedev.chatchat.domain.MessageData
 import com.gyleedev.chatchat.domain.MessageSendState
 import kotlinx.coroutines.Dispatchers
@@ -9,24 +8,26 @@ import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class SendMessageUseCase @Inject constructor(
-    private val repository: MessageRepository
+    private val insertMessageToLocalUseCase: InsertMessageToLocalUseCase,
+    private val sendMessageToRemoteUseCase: SendMessageToRemoteUseCase,
+    private val updateMessageStateUseCase: UpdateMessageStateUseCase
 ) {
     suspend operator fun invoke(messageData: MessageData, rid: Long, networkState: Boolean) {
         withContext(Dispatchers.IO) {
-            val messageId = repository.insertMessageToLocal(messageData, rid)
+            val messageId = insertMessageToLocalUseCase(messageData, rid)
             if (networkState) {
                 val request = try {
-                    repository.insertMessageToRemote(messageData).first()
+                    sendMessageToRemoteUseCase(messageData).first()
                 } catch (e: Throwable) {
                     println(e)
                 }
                 if (request is MessageSendState) {
                     val message = messageData.copy(messageSendState = request)
-                    repository.updateMessageState(messageId, rid, message)
+                    updateMessageStateUseCase(messageId, rid, message)
                 }
             } else {
                 val message = messageData.copy(messageSendState = MessageSendState.FAIL)
-                repository.updateMessageState(messageId, rid, message)
+                updateMessageStateUseCase(messageId, rid, message)
             }
         }
     }
