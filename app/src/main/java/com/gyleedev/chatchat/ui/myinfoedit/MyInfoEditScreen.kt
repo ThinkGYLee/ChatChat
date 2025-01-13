df521bffa041a9a9a632a7c8b2820694874db8e8
ThinkGYLee <ntnpholicfp@gmail.com>
2025-01-13 23:59:18 +0900

    - MyInfoEdit 관련 코드 구현

--- app/src/main/java/com/gyleedev/chatchat/ui/myinfoedit/MyInfoEditScreen.kt
+++ app/src/main/java/com/gyleedev/chatchat/ui/myinfoedit/MyInfoEditScreen.kt
@@ -1,32 +1,40 @@
 package com.gyleedev.chatchat.ui.myinfoedit
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.sizeIn
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.ArrowBack
-import androidx.compose.material.icons.outlined.Edit
-import androidx.compose.material.icons.outlined.Email
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -42,11 +50,12 @@ import com.skydoves.landscapist.placeholder.shimmer.ShimmerPlugin
 @Composable
 fun MyInfoEditScreen(
     onBackKeyPressed: () -> Unit,
-    onChatRoomClick: (String) -> Unit,
     modifier: Modifier = Modifier,
     viewModel: MyInfoEditViewModel = hiltViewModel()
 ) {
-    val userData by viewModel.userData.collectAsStateWithLifecycle()
+    val myData by viewModel.myUserData.collectAsStateWithLifecycle()
+    val myName by viewModel.myNameQuery.collectAsStateWithLifecycle()
+    val myStatus by viewModel.myStatusQuery.collectAsStateWithLifecycle()
 
     Scaffold(
         modifier = modifier,
@@ -60,6 +69,11 @@ fun MyInfoEditScreen(
                             contentDescription = "Arrow Back button"
                         )
                     }
+                },
+                actions = {
+                    TextButton(onClick = {}, enabled = myName.isNotEmpty()) {
+                        Text("완료")
+                    }
                 }
             )
         }
@@ -68,13 +82,14 @@ fun MyInfoEditScreen(
             modifier = Modifier
                 .padding(innerPadding)
                 .consumeWindowInsets(innerPadding)
-                .fillMaxSize(),
+                .fillMaxSize()
+                .padding(horizontal = 60.dp),
             verticalArrangement = Arrangement.Bottom,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             GlideImage(
                 imageModel = {
-                    userData?.picture?.ifBlank { R.drawable.icons8__ }
+                    myData?.picture?.ifBlank { R.drawable.icons8__ }
                 },
                 modifier = Modifier
                     .sizeIn(
@@ -91,32 +106,97 @@ fun MyInfoEditScreen(
                 },
                 previewPlaceholder = painterResource(id = R.drawable.icons8__)
             )
-            Spacer(modifier = Modifier.height(20.dp))
-            Text(text = userData?.name ?: "anonymous")
-            Spacer(modifier = Modifier.height(8.dp))
-            Text(text = userData?.status ?: "")
             Spacer(modifier = Modifier.height(60.dp))
-            Row(modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Absolute.SpaceEvenly) {
-                Column(
-                    modifier = Modifier.clickable {
-                    },
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Icon(imageVector = Icons.Outlined.Email, contentDescription = "message button")
-                    Spacer(modifier = Modifier.height(8.dp))
-                    Text(text = "채팅하기")
+            Column(horizontalAlignment = Alignment.Start) {
+                Text(
+                    "이름을 입력하세요",
+                    modifier = Modifier.padding(horizontal = 16.dp),
+                    style = MaterialTheme.typography.labelSmall
+                )
+                Row {
+                    MyEditTextField(
+                        myName,
+                        onValueChange = { viewModel.changeNameQuery(it) },
+                        onReset = { viewModel.changeNameQuery("") })
                 }
-                Column(
-                    modifier = Modifier.clickable {
-                    },
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Icon(imageVector = Icons.Outlined.Edit, contentDescription = "edit button")
-                    Spacer(modifier = Modifier.height(8.dp))
-                    Text(text = "프로필 편집")
+                Spacer(modifier = Modifier.height(20.dp))
+                Text(
+                    "상태를 입력하세요",
+                    modifier = Modifier.padding(horizontal = 16.dp),
+                    style = MaterialTheme.typography.labelSmall
+                )
+                Row {
+                    MyEditTextField(
+                        myStatus,
+                        onValueChange = { viewModel.changeStatusQuery(it) },
+                        onReset = { viewModel.changeStatusQuery("") })
                 }
             }
-            Spacer(modifier = Modifier.height(72.dp))
+            Spacer(modifier = Modifier.height(160.dp))
+        }
+    }
+}
+
+@Composable
+fun MyEditTextField(
+    query: String,
+    onValueChange: (String) -> Unit,
+    onReset: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    var alpha by remember { mutableFloatStateOf(1f) }
+
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Column {
+            BasicTextField(
+                modifier = Modifier
+                    .background(
+                        color = MaterialTheme.colorScheme.surface,
+                        shape = RoundedCornerShape(8.dp)
+                    )
+                    .onFocusChanged {
+                        alpha = if (it.isFocused) 0.6f else 1f
+                    }
+                    .padding(horizontal = 16.dp),
+                value = query,
+                onValueChange = onValueChange,
+                singleLine = true,
+                textStyle = MaterialTheme.typography.bodyLarge.copy(color = MaterialTheme.colorScheme.onSurface),
+                decorationBox = { innerTextField ->
+                    Row(
+                        modifier = Modifier
+                            .padding(vertical = 12.dp),
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Box(modifier = Modifier.weight(10f)) {
+                            if (query.isEmpty()) {
+                                Text(
+                                    text = "상태를 입력하세요",
+                                    style = MaterialTheme.typography.bodyLarge,
+                                    color = Color(0xFF848484),
+                                    modifier = Modifier
+                                        .padding(start = 4.dp)
+                                        .align(Alignment.CenterStart)
+                                )
+                            }
+                            Row(modifier = Modifier.align(Alignment.CenterStart)) {
+                                innerTextField()
+                            }
+                        }
+                        if (query.isNotEmpty()) {
+                            Icon(
+                                imageVector = Icons.Filled.Close,
+                                contentDescription = null,
+                                modifier = Modifier.clickable { onReset() }
+                            )
+                        }
+                    }
+                })
+            Spacer(modifier = Modifier.height(4.dp))
+            HorizontalDivider(modifier = Modifier.padding(horizontal = 16.dp))
         }
     }
 }
--- app/src/main/java/com/gyleedev/chatchat/ui/myinfoedit/MyInfoEditViewModel.kt
+++ app/src/main/java/com/gyleedev/chatchat/ui/myinfoedit/MyInfoEditViewModel.kt
@@ -4,7 +4,6 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.viewModelScope
 import com.gyleedev.chatchat.core.BaseViewModel
 import com.gyleedev.chatchat.domain.UserData
-import com.gyleedev.chatchat.domain.usecase.GetFriendDataUseCase
 import com.gyleedev.chatchat.domain.usecase.GetMyUserDataUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -16,46 +15,40 @@ import javax.inject.Inject
 @HiltViewModel
 class MyInfoEditViewModel @Inject constructor(
     private val getMyUserDataUseCase: GetMyUserDataUseCase,
-    private val getFriendDataUseCase: GetFriendDataUseCase,
     savedStateHandle: SavedStateHandle
 ) : BaseViewModel() {
-    private val _userData = MutableStateFlow(UserData())
-    val userData: StateFlow<UserData?> = _userData
+    private val _myUserData = MutableStateFlow(UserData())
+    val myUserData: StateFlow<UserData?> = _myUserData
 
-    private val _isUserDataMe = MutableStateFlow<Boolean?>(null)
-    val isUserDataMe: StateFlow<Boolean?> = _isUserDataMe
+    private val _myNameQuery = MutableStateFlow("")
+    val myNameQuery: StateFlow<String> = _myNameQuery
 
-    private val _isEditable = MutableStateFlow(false)
-    val isEditable: StateFlow<Boolean> = _isEditable
+    private val _myStatusQuery = MutableStateFlow("")
+    val myStatusQuery: StateFlow<String> = _myStatusQuery
 
     init {
         viewModelScope.launch {
-            val userUid = savedStateHandle.get<String>("user")
+            val userUid = savedStateHandle.get<String>("myInfo")
             val myUserData = getMyUserDataUseCase().first()
             if (myUserData != null && userUid != null) {
                 if (userUid == myUserData.uid) {
-                    _userData.emit(myUserData)
-                    _isUserDataMe.emit(true)
-                } else {
-                    val friendData = getFriendDataUseCase(userUid).first()
-                    _userData.emit(
-                        UserData(
-                            uid = friendData.uid,
-                            picture = friendData.picture,
-                            email = friendData.email,
-                            status = friendData.status,
-                            name = friendData.name
-                        )
-                    )
-                    _isUserDataMe.emit(false)
+                    _myUserData.emit(myUserData)
+                    _myNameQuery.emit(myUserData.name)
+                    _myStatusQuery.emit(myUserData.status)
                 }
             }
         }
     }
 
-    fun changeEditModeStatus() {
+    fun changeNameQuery(query: String) {
         viewModelScope.launch {
-            _isEditable.emit(!_isEditable.value)
+            _myNameQuery.emit(query)
+        }
+    }
+
+    fun changeStatusQuery(query: String) {
+        viewModelScope.launch {
+            _myStatusQuery.emit(query)
         }
     }
 }
