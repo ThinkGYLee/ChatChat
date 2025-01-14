aebe5a72e8b8a6e3e195d25e6ceb3b03f40b33a3
ThinkGYLee <ntnpholicfp@gmail.com>
2025-01-14 23:17:43 +0900

    - MyInfoViewModel  - 필요 없는 로직 제거  - updateUser 관련 로직 추가

--- app/src/main/java/com/gyleedev/chatchat/ui/myinfo/MyInfoViewModel.kt
+++ app/src/main/java/com/gyleedev/chatchat/ui/myinfo/MyInfoViewModel.kt
@@ -4,7 +4,6 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.viewModelScope
 import com.gyleedev.chatchat.core.BaseViewModel
 import com.gyleedev.chatchat.domain.UserData
-import com.gyleedev.chatchat.domain.usecase.GetFriendDataUseCase
 import com.gyleedev.chatchat.domain.usecase.GetMyUserDataUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -16,17 +15,10 @@ import javax.inject.Inject
 @HiltViewModel
 class MyInfoViewModel @Inject constructor(
     private val getMyUserDataUseCase: GetMyUserDataUseCase,
-    private val getFriendDataUseCase: GetFriendDataUseCase,
     savedStateHandle: SavedStateHandle
 ) : BaseViewModel() {
     private val _userData = MutableStateFlow(UserData())
     val userData: StateFlow<UserData?> = _userData
-
-    private val _isUserDataMe = MutableStateFlow<Boolean?>(null)
-    val isUserDataMe: StateFlow<Boolean?> = _isUserDataMe
-
-    private val _isEditable = MutableStateFlow(false)
-    val isEditable: StateFlow<Boolean> = _isEditable
 
     init {
         viewModelScope.launch {
@@ -35,27 +27,17 @@ class MyInfoViewModel @Inject constructor(
             if (myUserData != null && userUid != null) {
                 if (userUid == myUserData.uid) {
                     _userData.emit(myUserData)
-                    _isUserDataMe.emit(true)
-                } else {
-                    val friendData = getFriendDataUseCase(userUid).first()
-                    _userData.emit(
-                        UserData(
-                            uid = friendData.uid,
-                            picture = friendData.picture,
-                            email = friendData.email,
-                            status = friendData.status,
-                            name = friendData.name
-                        )
-                    )
-                    _isUserDataMe.emit(false)
                 }
             }
         }
     }
 
-    fun changeEditModeStatus() {
+    fun updateUser() {
         viewModelScope.launch {
-            _isEditable.emit(!_isEditable.value)
+            val myUserData = getMyUserDataUseCase().first()
+            if (myUserData != null) {
+                _userData.emit(myUserData)
+            }
         }
     }
 }
