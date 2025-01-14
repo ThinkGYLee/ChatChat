7c8c32e6ecbbe17b7f94ccffdfccd07052388f25
ThinkGYLee <ntnpholicfp@gmail.com>
2025-01-15 05:20:26 +0900

    - MainViewModel  - friendList 업데이트 로직 추가

--- app/src/main/java/com/gyleedev/chatchat/ui/MainViewModel.kt
+++ app/src/main/java/com/gyleedev/chatchat/ui/MainViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.gyleedev.chatchat.data.repository.UserRepository
 import com.gyleedev.chatchat.domain.UserState
+import com.gyleedev.chatchat.domain.usecase.UpdateFriendListUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -12,7 +13,8 @@ import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
-    private val userRepository: UserRepository
+    private val userRepository: UserRepository,
+    private val updateFriendListUseCase: UpdateFriendListUseCase
 ) : ViewModel() {
     private val _isUserExists = MutableStateFlow(UserState.Loading)
     val isUserExists: StateFlow<UserState> = _isUserExists
@@ -21,18 +23,19 @@ class MainViewModel @Inject constructor(
     val startDestination: StateFlow<String> = _startDestination
 
     init {
-        fetchUserExists()
+        viewModelScope.launch {
+            fetchUserExists()
+            updateFriendListUseCase()
+        }
     }
 
-    private fun fetchUserExists() {
-        viewModelScope.launch {
-            if (userRepository.fetchUserExists()) {
-                _isUserExists.emit(UserState.Exists)
-                _startDestination.emit(FRIENDLIST)
-            } else {
-                _isUserExists.emit(UserState.NoUser)
-                _startDestination.emit(LOGIN)
-            }
+    private suspend fun fetchUserExists() {
+        if (userRepository.fetchUserExists()) {
+            _startDestination.emit(FRIENDLIST)
+            _isUserExists.emit(UserState.Exists)
+        } else {
+            _isUserExists.emit(UserState.NoUser)
+            _startDestination.emit(LOGIN)
         }
     }
 }
