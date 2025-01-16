27c377778b7102459c42cb666446b10431fa075c
ThinkGYLee <ntnpholicfp@gmail.com>
2025-01-16 22:18:43 +0900

    - UpdateFriendInfoUseCase 추가

--- app/src/main/java/com/gyleedev/chatchat/domain/usecase/UpdateFriendInfoUseCase.kt
+++ app/src/main/java/com/gyleedev/chatchat/domain/usecase/UpdateFriendInfoUseCase.kt
@@ -1,13 +1,12 @@
 package com.gyleedev.chatchat.domain.usecase
 
 import com.gyleedev.chatchat.data.repository.UserRepository
-import kotlinx.coroutines.flow.first
 import javax.inject.Inject
 
 class UpdateFriendInfoUseCase @Inject constructor(
     private val repository: UserRepository
 ) {
-    suspend operator fun invoke() {
-
+    suspend operator fun invoke(uid: String) {
+        repository.updateFriendInfoByUid(uid)
     }
 }
