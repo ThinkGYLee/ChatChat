0219c87019ea57ff08a95cd72dbd5884cf7db3de
ThinkGYLee <ntnpholicfp@gmail.com>
2025-01-16 22:20:04 +0900

    - FriendInfoViewModel  - user라는 명칭을 friend로 변경  - local에서만 가져오는 로직을 리모트와 비교해서 업데이트 하도록 변경

--- app/src/main/java/com/gyleedev/chatchat/ui/friendinfo/FriendInfoViewModel.kt
+++ app/src/main/java/com/gyleedev/chatchat/ui/friendinfo/FriendInfoViewModel.kt
@@ -3,8 +3,9 @@ package com.gyleedev.chatchat.ui.friendinfo
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.viewModelScope
 import com.gyleedev.chatchat.core.BaseViewModel
-import com.gyleedev.chatchat.domain.UserData
+import com.gyleedev.chatchat.domain.FriendData
 import com.gyleedev.chatchat.domain.usecase.GetFriendDataUseCase
+import com.gyleedev.chatchat.domain.usecase.UpdateFriendInfoUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
@@ -15,24 +16,23 @@ import javax.inject.Inject
 @HiltViewModel
 class FriendInfoViewModel @Inject constructor(
     private val getFriendDataUseCase: GetFriendDataUseCase,
+    private val updateFriendInfoUseCase: UpdateFriendInfoUseCase,
     savedStateHandle: SavedStateHandle
 ) : BaseViewModel() {
-    private val _userData = MutableStateFlow(UserData())
-    val userData: StateFlow<UserData?> = _userData
+    private val _friendData = MutableStateFlow(FriendData())
+    val friendData: StateFlow<FriendData?> = _friendData
 
     init {
         viewModelScope.launch {
             val userUid = savedStateHandle.get<String>("friend")
             if (userUid != null) {
                 val friendData = getFriendDataUseCase(userUid).first()
-                _userData.emit(
-                    UserData(
-                        uid = friendData.uid,
-                        picture = friendData.picture,
-                        email = friendData.email,
-                        status = friendData.status,
-                        name = friendData.name
-                    )
+                _friendData.emit(
+                    friendData
+                )
+                updateFriendInfoUseCase(friendData.uid)
+                _friendData.emit(
+                    getFriendDataUseCase(friendData.uid).first()
                 )
             }
         }
