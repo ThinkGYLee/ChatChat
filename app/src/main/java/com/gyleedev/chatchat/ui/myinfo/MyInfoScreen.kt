01dff3024befcbc0caacc548a47a5aca4f68db2b
ThinkGYLee <ntnpholicfp@gmail.com>
2025-01-25 23:50:18 +0900

    - MyInfoScreen  - 메세지 버튼 설명 적용

--- app/src/main/java/com/gyleedev/chatchat/ui/myinfo/MyInfoScreen.kt
+++ app/src/main/java/com/gyleedev/chatchat/ui/myinfo/MyInfoScreen.kt
@@ -28,6 +28,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.Lifecycle
@@ -109,7 +110,7 @@ fun MyInfoScreen(
                     IconButton(onClick = { userData?.uid?.let { onChatRoomClick(it) } }) {
                         Icon(
                             imageVector = Icons.Outlined.Email,
-                            contentDescription = "message button"
+                            contentDescription = stringResource(R.string.message_button_description)
                         )
                     }
                     Icon(imageVector = Icons.Outlined.Email, contentDescription = "message button")
