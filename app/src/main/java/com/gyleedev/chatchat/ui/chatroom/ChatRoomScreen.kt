678feb8fd40bef6f8e854bf46e68194587f66434
ThinkGYLee <ntnpholicfp@gmail.com>
2025-02-03 23:58:56 +0900

    - ChatRoomScreen  - 애로우백버튼 설명 스트링 리소스로 변경

--- app/src/main/java/com/gyleedev/chatchat/ui/chatroom/ChatRoomScreen.kt
+++ app/src/main/java/com/gyleedev/chatchat/ui/chatroom/ChatRoomScreen.kt
@@ -44,6 +44,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -52,6 +53,7 @@ import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.flowWithLifecycle
 import androidx.paging.compose.collectAsLazyPagingItems
+import com.gyleedev.chatchat.R
 import com.gyleedev.chatchat.domain.MessageData
 import com.gyleedev.chatchat.domain.MessageSendState
 import com.gyleedev.chatchat.ui.theme.ChatChatTheme
@@ -111,7 +113,7 @@ fun ChatRoomScreen(
                         ) {
                             Icon(
                                 imageVector = Icons.AutoMirrored.Filled.ArrowBack,
-                                contentDescription = "arrow back button"
+                                contentDescription = stringResource(R.string.navigation_arrow_back_icon_description)
                             )
                         }
                     }
