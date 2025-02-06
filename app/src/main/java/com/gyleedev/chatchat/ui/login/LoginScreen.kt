05c40004eb37bf5ef6754f7ce994e74f73d65de3
ThinkGYLee <ntnpholicfp@gmail.com>
2025-02-06 23:57:58 +0900

    - LoginScreen  - 텍스트 전체 스트링 리소스로 전환

--- app/src/main/java/com/gyleedev/chatchat/ui/login/LoginScreen.kt
+++ app/src/main/java/com/gyleedev/chatchat/ui/login/LoginScreen.kt
@@ -49,6 +49,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -221,7 +222,7 @@ fun LoginBox(
     ) {
         Image(
             painter = painterResource(id = icon),
-            contentDescription = "google icon",
+            contentDescription = stringResource(R.string.google_icon_description),
             modifier = Modifier.size(24.dp)
         )
         Text(text = "$name 으로 시작하기")
