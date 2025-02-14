bcad7ba2ab2882c6a8b0c6f53580c9311440a2a4
ThinkGYLee <ntnpholicfp@gmail.com>
2025-02-14 23:24:29 +0900

    - FriendListScreen  - Glide 이미지 사이즈 지정  - PhotoPicker 추가

--- app/src/main/java/com/gyleedev/chatchat/ui/friendlist/FriendListScreen.kt
+++ app/src/main/java/com/gyleedev/chatchat/ui/friendlist/FriendListScreen.kt
@@ -10,8 +10,10 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -25,6 +27,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -35,7 +41,11 @@ import androidx.paging.compose.collectAsLazyPagingItems
 import com.gyleedev.chatchat.R
 import com.gyleedev.chatchat.domain.FriendData
 import com.gyleedev.chatchat.domain.UserData
+import com.skydoves.landscapist.ImageOptions
+import com.skydoves.landscapist.components.rememberImageComponent
 import com.skydoves.landscapist.glide.GlideImage
+import com.skydoves.landscapist.placeholder.shimmer.Shimmer
+import com.skydoves.landscapist.placeholder.shimmer.ShimmerPlugin
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -133,7 +143,27 @@ fun MyUserData(
             .clickable(onClick = onClick),
         verticalAlignment = Alignment.CenterVertically
     ) {
-        GlideImage(imageModel = { userData.picture.ifBlank { R.drawable.icons8__ } })
+        GlideImage(
+            imageModel = {
+                userData.picture.ifBlank { R.drawable.icons8__ }
+            },
+            imageOptions = ImageOptions(
+                contentScale = ContentScale.Crop
+            ),
+            modifier = Modifier
+                .size(60.dp)
+                .clip(RoundedCornerShape(20.dp)),
+            component = rememberImageComponent {
+                +ShimmerPlugin(
+                    Shimmer.Flash(
+                        baseColor = Color.White,
+                        highlightColor = Color.LightGray
+                    )
+                )
+            },
+            previewPlaceholder = painterResource(id = R.drawable.icons8__)
+        )
+        //GlideImage(imageModel = { userData.picture.ifBlank { R.drawable.icons8__ } })
         Spacer(modifier = Modifier.width(20.dp))
         Column(verticalArrangement = Arrangement.Center) {
             Text(text = userData.name)
