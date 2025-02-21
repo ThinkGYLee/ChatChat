cfadf4dbace17d84463c01b5690b8070bba67f7f
ThinkGYLee <ntnpholicfp@gmail.com>
2025-02-22 02:45:32 +0900

    - DetectUtil 추가

---
+++ app/src/main/java/com/gyleedev/chatchat/util/DetectUrl.kt
@@ -0,0 +1,21 @@
+package com.gyleedev.chatchat.util
+
+import android.text.SpannableString
+import android.text.util.Linkify
+import android.util.Patterns
+
+fun detectUrl(comment: String): String {
+    val spannableString = SpannableString.valueOf(comment)
+    val matchFilter = Linkify.sUrlMatchFilter
+    val pattern = Patterns.WEB_URL
+    var url: String = ""
+    val m = pattern.matcher(spannableString)
+    while (m.find()) {
+        val start = m.start()
+        val end = m.end()
+        if (matchFilter == null || matchFilter.acceptMatch(spannableString, start, end)) {
+            url = spannableString.subSequence(start, end).toString()
+        }
+    }
+    return url
+}
