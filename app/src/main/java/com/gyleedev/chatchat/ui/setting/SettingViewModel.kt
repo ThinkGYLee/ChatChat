4ad1146fcc11fd46f19902f6a79e66db813e2901
ThinkGYLee <ntnpholicfp@gmail.com>
2025-04-03 01:50:02 +0900

    - SettingViewModel  - 로그아웃 함수 추가

--- app/src/main/java/com/gyleedev/chatchat/ui/setting/SettingViewModel.kt
+++ app/src/main/java/com/gyleedev/chatchat/ui/setting/SettingViewModel.kt
@@ -1,8 +1,16 @@
 package com.gyleedev.chatchat.ui.setting
 
 import com.gyleedev.chatchat.core.BaseViewModel
+import com.gyleedev.chatchat.domain.usecase.LogoutUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class SettingViewModel @Inject constructor() : BaseViewModel()
+class SettingViewModel @Inject constructor(
+    private val logoutUseCase: LogoutUseCase
+) : BaseViewModel() {
+
+    fun logout() {
+        logoutUseCase
+    }
+}
