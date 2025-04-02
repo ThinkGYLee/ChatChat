17d6eb038b47ed18c7a1f6fc45d2819590b64b1b
ThinkGYLee <ntnpholicfp@gmail.com>
2025-04-02 23:50:02 +0900

    - SettingScreen  - logout 버튼 추가

--- app/src/main/java/com/gyleedev/chatchat/ui/setting/SettingScreen.kt
+++ app/src/main/java/com/gyleedev/chatchat/ui/setting/SettingScreen.kt
@@ -7,4 +7,9 @@ import androidx.compose.ui.Modifier
 fun SettingScreen(
     modifier: Modifier = Modifier
 ) {
+    androidx.compose.material3.Button(
+        onClick = {}
+    ) {
+        androidx.compose.material3.Text("log out")
+    }
 }
