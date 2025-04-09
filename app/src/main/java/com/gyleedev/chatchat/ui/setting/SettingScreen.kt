7e932573ad805f9573153cadf1c63bf177e1b9e9
ThinkGYLee <ntnpholicfp@gmail.com>
2025-04-09 23:23:30 +0900

    - SettingScreen  - logout dialog 추가  - viewModel 추가

--- app/src/main/java/com/gyleedev/chatchat/ui/setting/SettingScreen.kt
+++ app/src/main/java/com/gyleedev/chatchat/ui/setting/SettingScreen.kt
@@ -1,15 +1,82 @@
 package com.gyleedev.chatchat.ui.setting
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.AlertDialogDefaults
+import androidx.compose.material3.BasicAlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SettingScreen(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    viewModel: SettingViewModel = hiltViewModel()
 ) {
-    androidx.compose.material3.Button(
-        onClick = {}
-    ) {
-        androidx.compose.material3.Text("log out")
+    var openDialog by remember { mutableStateOf(false) }
+
+    Scaffold(
+        modifier = modifier
+    ) { innerPadding ->
+        Column(modifier = Modifier.padding(innerPadding)) {
+            Button(
+                onClick = {
+                    openDialog = true
+                }
+            ) {
+                Text("log out")
+            }
+        }
+        if (openDialog) {
+            BasicAlertDialog(
+                onDismissRequest = {
+                    openDialog = false
+                },
+                content = {
+                    Surface(
+                        modifier = Modifier.wrapContentSize(),
+                        shape = MaterialTheme.shapes.large,
+                        tonalElevation = AlertDialogDefaults.TonalElevation
+                    ) {
+                        Column(modifier = Modifier.padding(16.dp)) {
+                            Text(
+                                text =
+                                "This area typically contains the supportive text " +
+                                        "which presents the details regarding the Dialog's purpose.",
+                            )
+                            Spacer(modifier = Modifier.height(24.dp))
+                            TextButton(
+                                onClick = {
+                                    openDialog = false
+                                    viewModel.logout()
+                                },
+                                modifier = Modifier.align(Alignment.End)
+                            ) {
+                                Text("Confirm")
+                            }
+                        }
+                    }
+
+                },
+                modifier = Modifier.wrapContentSize()
+            )
+        }
     }
 }
