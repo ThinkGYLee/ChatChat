4849c27219cbe729111c1070a3dae29dc7b9fa6b
ThinkGYLee <ntnpholicfp@gmail.com>
2025-04-12 21:12:02 +0900

    - FriendListScreen  - FriendData 클릭방식 나눔  - FriendDialog 구현

--- app/src/main/java/com/gyleedev/chatchat/ui/friendlist/FriendListScreen.kt
+++ app/src/main/java/com/gyleedev/chatchat/ui/friendlist/FriendListScreen.kt
@@ -1,6 +1,8 @@
 package com.gyleedev.chatchat.ui.friendlist
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.combinedClickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -12,21 +14,28 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.Add
+import androidx.compose.material3.AlertDialogDefaults
+import androidx.compose.material3.BasicAlertDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -73,6 +82,8 @@ fun FriendListScreen(
 
     val myUserData = viewModel.myUserData.collectAsStateWithLifecycle()
     val items = viewModel.items.collectAsLazyPagingItems()
+
+    var openFriendDialog by remember { mutableStateOf(false) }
 
     Scaffold(
         topBar = {
@@ -128,9 +139,19 @@ fun FriendListScreen(
                         contentType = { 0 }
                     ) { index ->
                         val friend = items[index] as FriendData
-                        FriendData(onClick = { onFriendClick(friend.uid) }, friendData = friend)
+                        FriendData(
+                            onClick = { onFriendClick(friend.uid) },
+                            onLongClick = { openFriendDialog = true },
+                            friendData = friend
+                        )
                     }
                 }
+            }
+
+            if (openFriendDialog) {
+                FriendDialog(
+                    closeDialog = { openFriendDialog = false }
+                )
             }
         }
     }
@@ -189,9 +210,11 @@ fun MyUserData(
     }
 }
 
+@OptIn(ExperimentalFoundationApi::class)
 @Composable
 fun FriendData(
     onClick: () -> Unit,
+    onLongClick: () -> Unit,
     friendData: FriendData,
     modifier: Modifier = Modifier
 ) {
@@ -205,7 +228,7 @@ fun FriendData(
         modifier = modifier
             .fillMaxWidth()
             .padding(vertical = 8.dp)
-            .clickable(onClick = onClick),
+            .combinedClickable(onLongClick = { onLongClick() }, onClick = { onClick() }),
         verticalAlignment = Alignment.CenterVertically
     ) {
         GlideImage(
@@ -239,4 +262,42 @@ fun FriendData(
             }
         }
     }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FriendDialog(
+    closeDialog: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    BasicAlertDialog(
+        onDismissRequest = {
+            closeDialog()
+        },
+        content = {
+            Surface(
+                modifier = Modifier.wrapContentSize(),
+                shape = MaterialTheme.shapes.medium,
+                tonalElevation = AlertDialogDefaults.TonalElevation
+            ) {
+                Column(
+                    modifier = Modifier.padding(24.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    TextButton(onClick = { closeDialog() }) {
+                        Text("친구 차단")
+                    }
+                    HorizontalDivider()
+                    TextButton(onClick = { closeDialog() }) {
+                        Text("친구 삭제")
+                    }
+                    HorizontalDivider()
+                    TextButton(onClick = { closeDialog() }) {
+                        Text("친구 숨김")
+                    }
+                }
+            }
+        },
+        modifier = modifier.wrapContentSize()
+    )
 }
