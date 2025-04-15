2c4d45d9553cf3857265636e33fbdd77dfe22f17
ThinkGYLee <ntnpholicfp@gmail.com>
2025-04-15 23:24:05 +0900

    - FriendListViewModel  - deleteFriend 함수 추가  - deleteFriend에 전달된 값이 null일때 noSuchUserAlert SharedFlow로 emit

--- app/src/main/java/com/gyleedev/chatchat/ui/friendlist/FriendListViewModel.kt
+++ app/src/main/java/com/gyleedev/chatchat/ui/friendlist/FriendListViewModel.kt
@@ -3,14 +3,18 @@ package com.gyleedev.chatchat.ui.friendlist
 import androidx.lifecycle.viewModelScope
 import androidx.paging.cachedIn
 import com.gyleedev.chatchat.core.BaseViewModel
+import com.gyleedev.chatchat.domain.FriendData
 import com.gyleedev.chatchat.domain.UserData
 import com.gyleedev.chatchat.domain.usecase.AddFriendsUseCase
+import com.gyleedev.chatchat.domain.usecase.DeleteFriendUseCase
 import com.gyleedev.chatchat.domain.usecase.GetFriendsCountUseCase
 import com.gyleedev.chatchat.domain.usecase.GetFriendsUseCase
 import com.gyleedev.chatchat.domain.usecase.GetMyFriendFromRemoteUseCase
 import com.gyleedev.chatchat.domain.usecase.GetMyUserDataUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
@@ -22,13 +26,17 @@ class FriendListViewModel @Inject constructor(
     getFriendsUseCase: GetFriendsUseCase,
     private val getMyFriendFromRemoteUseCase: GetMyFriendFromRemoteUseCase,
     private val addFriendsUseCase: AddFriendsUseCase,
-    private val getFriendsCountUseCase: GetFriendsCountUseCase
+    private val getFriendsCountUseCase: GetFriendsCountUseCase,
+    private val deleteFriendUseCase: DeleteFriendUseCase
 ) : BaseViewModel() {
 
     private val _myUserData = MutableStateFlow<UserData?>(null)
     val myUserData: StateFlow<UserData?> = _myUserData
 
     val items = getFriendsUseCase().cachedIn(viewModelScope)
+
+    private val _noSuchUserAlert = MutableSharedFlow<Unit>()
+    val noSuchUserAlert: SharedFlow<Unit> = _noSuchUserAlert
 
     init {
         viewModelScope.launch {
@@ -61,5 +69,15 @@ class FriendListViewModel @Inject constructor(
 
     private suspend fun addMyFriendsToLocal(friends: List<UserData>) {
         addFriendsUseCase(friends)
+    }
+
+    fun deleteFriend(friendData: FriendData?) {
+        viewModelScope.launch {
+            if (friendData != null) {
+                deleteFriendUseCase(friendData)
+            } else {
+                _noSuchUserAlert.emit(Unit)
+            }
+        }
     }
 }
