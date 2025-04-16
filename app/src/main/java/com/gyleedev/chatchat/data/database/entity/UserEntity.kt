4eeeb56a0d00dc4c8ba0c6760ec0c11c754b45a7
ThinkGYLee <ntnpholicfp@gmail.com>
2025-04-17 01:41:27 +0900

    - UserEntity 추가

---
+++ app/src/main/java/com/gyleedev/chatchat/data/database/entity/UserEntity.kt
@@ -0,0 +1,75 @@
+package com.gyleedev.chatchat.data.database.entity
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.gyleedev.chatchat.domain.RelatedUseLocalData
+import com.gyleedev.chatchat.domain.UserData
+import com.gyleedev.chatchat.domain.UserRelationState
+
+@Entity(
+    tableName = "user"
+)
+data class UserEntity(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    val id: Long,
+    @ColumnInfo(name = "email")
+    val email: String,
+    @ColumnInfo(name = "name")
+    val name: String,
+    @ColumnInfo(name = "uid")
+    val uid: String,
+    @ColumnInfo(name = "picture")
+    val picture: String,
+    @ColumnInfo(name = "status")
+    val status: String,
+    @ColumnInfo(name = "relation")
+    val relation: UserRelationState
+)
+
+fun RelatedUseLocalData.toEntity(): UserEntity {
+    return UserEntity(
+        id = 0,
+        name = name,
+        email = email,
+        uid = uid,
+        picture = picture,
+        status = status,
+        relation = userRelation
+    )
+}
+
+fun UserEntity.toRelationLocalData(): RelatedUseLocalData {
+    return RelatedUseLocalData(
+        id = id,
+        name = name,
+        email = email,
+        uid = uid,
+        picture = picture,
+        status = status,
+        userRelation = relation
+    )
+}
+
+fun UserEntity.toModel(): UserData {
+    return UserData(
+        name = name,
+        email = email,
+        uid = uid,
+        picture = picture,
+        status = status
+    )
+}
+
+fun UserData.toEntityAsFriend(): UserEntity {
+    return UserEntity(
+        id = 0,
+        name = name,
+        email = email,
+        uid = uid,
+        picture = picture,
+        status = status,
+        relation = UserRelationState.FRIEND
+    )
+}
