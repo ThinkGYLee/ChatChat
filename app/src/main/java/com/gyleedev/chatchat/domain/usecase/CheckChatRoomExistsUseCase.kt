9b0e5585ae36f0222408fabe9e65ab60da050ffe
ThinkGYLee <ntnpholicfp@gmail.com>
2025-04-17 02:19:04 +0900

    - CheckChatRoomExistsUseCase  - 변수 변경에 따른 수정

--- app/src/main/java/com/gyleedev/chatchat/domain/usecase/CheckChatRoomExistsUseCase.kt
+++ app/src/main/java/com/gyleedev/chatchat/domain/usecase/CheckChatRoomExistsUseCase.kt
@@ -1,14 +1,14 @@
 package com.gyleedev.chatchat.domain.usecase
 
 import com.gyleedev.chatchat.data.repository.UserRepository
-import com.gyleedev.chatchat.domain.FriendData
+import com.gyleedev.chatchat.domain.RelatedUserLocalData
 import kotlinx.coroutines.flow.firstOrNull
 import javax.inject.Inject
 
 class CheckChatRoomExistsUseCase @Inject constructor(
     private val repository: UserRepository
 ) {
-    suspend operator fun invoke(friend: FriendData): Boolean {
+    suspend operator fun invoke(friend: RelatedUserLocalData): Boolean {
         return repository.checkChatRoomExistsInRemote(friend).firstOrNull() ?: false
     }
 }
