dc79f8a7e1607a9252900b3a15d605ea1c1d2f8e
ThinkGYLee <ntnpholicfp@gmail.com>
2025-04-17 02:20:40 +0900

    - GetChatRoomListUseCase  - 변수 변경에 따른 수정

--- app/src/main/java/com/gyleedev/chatchat/domain/usecase/GetChatRoomListUseCase.kt
+++ app/src/main/java/com/gyleedev/chatchat/domain/usecase/GetChatRoomListUseCase.kt
@@ -2,7 +2,7 @@ package com.gyleedev.chatchat.domain.usecase
 
 import androidx.paging.PagingData
 import com.gyleedev.chatchat.data.repository.UserRepository
-import com.gyleedev.chatchat.domain.ChatRoomDataWithFriend
+import com.gyleedev.chatchat.domain.ChatRoomDataWithRelatedUsers
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.withContext
@@ -11,7 +11,7 @@ import javax.inject.Inject
 class GetChatRoomListUseCase @Inject constructor(
     private val repository: UserRepository
 ) {
-    suspend operator fun invoke(): Flow<PagingData<ChatRoomDataWithFriend>> {
+    suspend operator fun invoke(): Flow<PagingData<ChatRoomDataWithRelatedUsers>> {
         return withContext(Dispatchers.IO) {
             repository.getChatRoomListFromLocal()
         }
