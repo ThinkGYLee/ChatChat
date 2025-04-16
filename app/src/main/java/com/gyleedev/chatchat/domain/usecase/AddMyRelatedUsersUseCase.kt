2c5d3e4439721f2c31d674683ace8ff14b6bf4ad
ThinkGYLee <ntnpholicfp@gmail.com>
2025-04-17 02:18:38 +0900

    - AddMyRelatedUsersUseCase  - AddFriendsUseCase.kt 에서 명칭 변경  - 변수, repository 함수 변경에 따른 수정

--- app/src/main/java/com/gyleedev/chatchat/domain/usecase/AddMyRelatedUsersUseCase.kt
+++ app/src/main/java/com/gyleedev/chatchat/domain/usecase/AddMyRelatedUsersUseCase.kt
@@ -1,17 +1,17 @@
 package com.gyleedev.chatchat.domain.usecase
 
+import com.gyleedev.chatchat.data.model.RelatedUserRemoteData
 import com.gyleedev.chatchat.data.repository.UserRepository
-import com.gyleedev.chatchat.domain.UserData
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
-class AddFriendsUseCase @Inject constructor(
+class AddMyRelatedUsersUseCase @Inject constructor(
     private val repository: UserRepository
 ) {
-    suspend operator fun invoke(friends: List<UserData>) {
+    suspend operator fun invoke(friends: List<RelatedUserRemoteData>) {
         return withContext(Dispatchers.IO) {
-            repository.insertMyFriendListToLocal(friends)
+            repository.insertMyRelationsToLocal(friends)
         }
     }
 }
