96606edbf074122055380d096de8e0a6fb19c6da
ThinkGYLee <ntnpholicfp@gmail.com>
2025-04-17 01:41:39 +0900

    - RelatedUserRemoteData 추가

---
+++ app/src/main/java/com/gyleedev/chatchat/data/model/RelatedUserRemoteData.kt
@@ -0,0 +1,27 @@
+package com.gyleedev.chatchat.data.model
+
+import com.google.gson.annotations.SerializedName
+import com.gyleedev.chatchat.domain.RelatedUserLocalData
+import com.gyleedev.chatchat.domain.UserRelationState
+
+// 기본값 설정 안해주면 crash남
+data class RelatedUserRemoteData(
+    @SerializedName("email") val email: String = "",
+    @SerializedName("name") val name: String = "",
+    @SerializedName("uid") val uid: String = "",
+    @SerializedName("picture") val picture: String = "",
+    @SerializedName("status") val status: String = "",
+    @SerializedName("userRelation") val userRelation: UserRelationState = UserRelationState.UNKNOWN
+)
+
+fun RelatedUserRemoteData.toRelatedUserLocalData(): RelatedUserLocalData {
+    return RelatedUserLocalData(
+        id = 0L,
+        email = email,
+        name = name,
+        uid = uid,
+        picture = picture,
+        status = status,
+        userRelation = userRelation
+    )
+}
