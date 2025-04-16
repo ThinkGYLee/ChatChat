44c83d7aa062ae2cf32b649943f2c09a745d8b74
ThinkGYLee <ntnpholicfp@gmail.com>
2025-04-17 02:21:03 +0900

    - GetFriendDataUseCase  - 변수 변경에 따른 수정

--- app/src/main/java/com/gyleedev/chatchat/domain/usecase/GetFriendDataUseCase.kt
+++ app/src/main/java/com/gyleedev/chatchat/domain/usecase/GetFriendDataUseCase.kt
@@ -1,14 +1,14 @@
 package com.gyleedev.chatchat.domain.usecase
 
 import com.gyleedev.chatchat.data.repository.UserRepository
-import com.gyleedev.chatchat.domain.FriendData
+import com.gyleedev.chatchat.domain.RelatedUserLocalData
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class GetFriendDataUseCase @Inject constructor(
     private val repository: UserRepository
 ) {
-    operator fun invoke(friend: String): Flow<FriendData> {
+    operator fun invoke(friend: String): Flow<RelatedUserLocalData> {
         return repository.getFriendById(friend)
     }
 }
