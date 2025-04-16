38f3ad53d6e63ec0c313ece82debdbab6ca8541c
ThinkGYLee <ntnpholicfp@gmail.com>
2025-04-17 01:43:10 +0900

    - FriendData를 RelatedUserLocalData로 변경

--- app/src/main/java/com/gyleedev/chatchat/domain/RelatedUserLocalData.kt
+++ app/src/main/java/com/gyleedev/chatchat/domain/RelatedUserLocalData.kt
@@ -3,11 +3,12 @@ package com.gyleedev.chatchat.domain
 import com.google.gson.annotations.SerializedName
 
 // 기본값 설정 안해주면 crash남
-data class FriendData(
+data class RelatedUserLocalData(
     @SerializedName("id") val id: Long = 0,
     @SerializedName("email") val email: String = "",
     @SerializedName("name") val name: String = "",
     @SerializedName("uid") val uid: String = "",
     @SerializedName("picture") val picture: String = "",
-    @SerializedName("status") val status: String = ""
+    @SerializedName("status") val status: String = "",
+    @SerializedName("userRelation") val userRelation: UserRelationState = UserRelationState.UNKNOWN
 )
