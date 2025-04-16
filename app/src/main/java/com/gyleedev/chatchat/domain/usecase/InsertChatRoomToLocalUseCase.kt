ffd668d09b46a369bed63afa33a698193e807fe2
ThinkGYLee <ntnpholicfp@gmail.com>
2025-04-17 02:21:45 +0900

    - InsertChatRoomToLocalUseCase  - 변수 변경에 따른 수정

--- app/src/main/java/com/gyleedev/chatchat/domain/usecase/InsertChatRoomToLocalUseCase.kt
+++ app/src/main/java/com/gyleedev/chatchat/domain/usecase/InsertChatRoomToLocalUseCase.kt
@@ -2,7 +2,7 @@ package com.gyleedev.chatchat.domain.usecase
 
 import com.gyleedev.chatchat.data.repository.UserRepository
 import com.gyleedev.chatchat.domain.ChatRoomData
-import com.gyleedev.chatchat.domain.FriendData
+import com.gyleedev.chatchat.domain.RelatedUserLocalData
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
@@ -10,9 +10,9 @@ import javax.inject.Inject
 class InsertChatRoomToLocalUseCase @Inject constructor(
     private val repository: UserRepository
 ) {
-    suspend operator fun invoke(friendData: FriendData, chatRoomData: ChatRoomData): Long {
+    suspend operator fun invoke(relatedUserLocalData: RelatedUserLocalData, chatRoomData: ChatRoomData): Long {
         return withContext(Dispatchers.IO) {
-            repository.insertChatRoomToLocal(friendData, chatRoomData)
+            repository.insertChatRoomToLocal(relatedUserLocalData, chatRoomData)
         }
     }
 }
