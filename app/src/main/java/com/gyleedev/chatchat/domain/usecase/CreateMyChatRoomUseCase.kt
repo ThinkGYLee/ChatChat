12414ad903ddbdaeefd8ab95bac491fa8d68c353
ThinkGYLee <ntnpholicfp@gmail.com>
2025-04-17 02:19:53 +0900

    - CreateMyChatRoomUseCase  - 변수 변경에 따른 수정

--- app/src/main/java/com/gyleedev/chatchat/domain/usecase/CreateMyChatRoomUseCase.kt
+++ app/src/main/java/com/gyleedev/chatchat/domain/usecase/CreateMyChatRoomUseCase.kt
@@ -2,7 +2,7 @@ package com.gyleedev.chatchat.domain.usecase
 
 import com.gyleedev.chatchat.data.repository.UserRepository
 import com.gyleedev.chatchat.domain.ChatRoomData
-import com.gyleedev.chatchat.domain.FriendData
+import com.gyleedev.chatchat.domain.RelatedUserLocalData
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
@@ -10,7 +10,7 @@ import javax.inject.Inject
 class CreateMyChatRoomUseCase @Inject constructor(
     private val repository: UserRepository
 ) {
-    suspend operator fun invoke(friend: FriendData, chatRoomData: ChatRoomData) {
+    suspend operator fun invoke(friend: RelatedUserLocalData, chatRoomData: ChatRoomData) {
         return withContext(Dispatchers.IO) {
             repository.createMyUserChatRoom(friend, chatRoomData)
         }
