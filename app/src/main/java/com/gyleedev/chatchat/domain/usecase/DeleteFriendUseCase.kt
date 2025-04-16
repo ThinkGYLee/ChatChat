8df60bedd7412411cb1d35b29377cb7b771d839d
ThinkGYLee <ntnpholicfp@gmail.com>
2025-04-17 02:29:37 +0900

    - DeleteFriendUseCase  - 함수 내용 변경  - 변수 변경  - 의미없는 디펜던시 제거

--- app/src/main/java/com/gyleedev/chatchat/domain/usecase/DeleteFriendUseCase.kt
+++ app/src/main/java/com/gyleedev/chatchat/domain/usecase/DeleteFriendUseCase.kt
@@ -1,15 +1,11 @@
 package com.gyleedev.chatchat.domain.usecase
 
 import com.gyleedev.chatchat.data.repository.UserRepository
-import com.gyleedev.chatchat.domain.ChatRoomData
-import com.gyleedev.chatchat.domain.DeleteFriendState
-import com.gyleedev.chatchat.domain.FriendData
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
+import com.gyleedev.chatchat.domain.RelatedUserLocalData
 import javax.inject.Inject
 
 class DeleteFriendUseCase @Inject constructor(
     private val repository: UserRepository
 ) {
-    suspend operator fun invoke(friend: FriendData) = repository.deleteFriendRequest(friend)
+    suspend operator fun invoke(friend: RelatedUserLocalData) = repository.deleteFriendRequest(friend)
 }
