36ae4ff549b0d9da53aac1c0c77aed660a57fcfd
ThinkGYLee <ntnpholicfp@gmail.com>
2025-04-18 22:48:23 +0900

    - FriendInfoViewModel  - hideFriendUseCase, blockFriendUseCase 추가

--- app/src/main/java/com/gyleedev/chatchat/ui/friendinfo/FriendInfoViewModel.kt
+++ app/src/main/java/com/gyleedev/chatchat/ui/friendinfo/FriendInfoViewModel.kt
@@ -4,8 +4,10 @@ import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.viewModelScope
 import com.gyleedev.chatchat.core.BaseViewModel
 import com.gyleedev.chatchat.domain.RelatedUserLocalData
+import com.gyleedev.chatchat.domain.usecase.BlockFriendUseCase
 import com.gyleedev.chatchat.domain.usecase.DeleteFriendUseCase
 import com.gyleedev.chatchat.domain.usecase.GetFriendDataUseCase
+import com.gyleedev.chatchat.domain.usecase.HideFriendUseCase
 import com.gyleedev.chatchat.domain.usecase.UpdateFriendInfoUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -19,6 +21,8 @@ class FriendInfoViewModel @Inject constructor(
     private val getFriendDataUseCase: GetFriendDataUseCase,
     private val updateFriendInfoUseCase: UpdateFriendInfoUseCase,
     private val deleteFriendUseCase: DeleteFriendUseCase,
+    private val hideFriendUseCase: HideFriendUseCase,
+    private val blockFriendUseCase: BlockFriendUseCase,
     savedStateHandle: SavedStateHandle
 ) : BaseViewModel() {
     private val _relatedUserLocalData = MutableStateFlow(RelatedUserLocalData())
