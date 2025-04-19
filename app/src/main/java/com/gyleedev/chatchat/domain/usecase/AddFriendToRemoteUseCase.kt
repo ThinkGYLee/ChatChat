0ffd3a21696ac0b83c6ec58be9ea4c6e5f6dd09d
ThinkGYLee <ntnpholicfp@gmail.com>
2025-04-19 23:22:56 +0900

    - AddFriendToRemoteUseCase 추가

---
+++ app/src/main/java/com/gyleedev/chatchat/domain/usecase/AddFriendToRemoteUseCase.kt
@@ -0,0 +1,13 @@
+package com.gyleedev.chatchat.domain.usecase
+
+import com.gyleedev.chatchat.data.repository.UserRepository
+import com.gyleedev.chatchat.domain.UserData
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+class AddFriendToRemoteUseCase @Inject constructor(
+    private val repository: UserRepository
+) {
+    suspend operator fun invoke(userData: UserData): Flow<Boolean> =
+        repository.addFriendToRemote(userData)
+}
