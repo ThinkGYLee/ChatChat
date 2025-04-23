e6e7a780197695c2981a45da9022407d5ebf2837
ThinkGYLee <ntnpholicfp@gmail.com>
2025-04-23 22:55:45 +0900

    - GetHideFriendsWithNameUseCase  - 주석 추가

--- app/src/main/java/com/gyleedev/chatchat/domain/usecase/GetHideFriendsWithNameUseCase.kt
+++ app/src/main/java/com/gyleedev/chatchat/domain/usecase/GetHideFriendsWithNameUseCase.kt
@@ -6,5 +6,7 @@ import javax.inject.Inject
 class GetHideFriendsWithNameUseCase @Inject constructor(
     private val repository: UserRepository
 ) {
+    //TODO fts4 관련 쿼리문 수정
+    //operator fun invoke(query: String) = repository.getHideFriendsWithFullTextName(query)
     operator fun invoke(query: String) = repository.getHideFriendsWithName(query)
 }
