0cd710bfc8cb8ce33afb653d453497a48e10b029
ThinkGYLee <ntnpholicfp@gmail.com>
2025-05-04 23:38:52 +0900

    - BlockUserUseCase - BlockFriendUseCase.kt 에서 명칭 변경

--- app/src/main/java/com/gyleedev/chatchat/domain/usecase/BlockUserUseCase.kt
+++ app/src/main/java/com/gyleedev/chatchat/domain/usecase/BlockUserUseCase.kt
@@ -4,8 +4,8 @@ import com.gyleedev.chatchat.data.repository.UserRepository
 import com.gyleedev.chatchat.domain.RelatedUserLocalData
 import javax.inject.Inject
 
-class BlockFriendUseCase @Inject constructor(
+class BlockUserUseCase @Inject constructor(
     private val repository: UserRepository
 ) {
-    suspend operator fun invoke(friend: RelatedUserLocalData) = repository.blockFriendRequest(friend)
+    suspend operator fun invoke(friend: RelatedUserLocalData) = repository.blockUserRequest(friend)
 }
