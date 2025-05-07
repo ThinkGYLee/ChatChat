c9f8df0087bf26aad8cdfc1a164659d4fa901573
ThinkGYLee <ntnpholicfp@gmail.com>
2025-05-07 22:34:32 +0900

    - FindUserScreen - Card 내부 ui 수정 - Card 에 친구 차단 버튼 추가

--- app/src/main/java/com/gyleedev/chatchat/ui/finduser/FindUserScreen.kt
+++ app/src/main/java/com/gyleedev/chatchat/ui/finduser/FindUserScreen.kt
@@ -3,6 +3,7 @@ package com.gyleedev.chatchat.ui.finduser
 import android.widget.Toast
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -12,7 +13,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
@@ -23,12 +25,15 @@ import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.outlined.ArrowBack
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -47,6 +52,7 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.focus.onFocusChanged
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
@@ -233,24 +239,26 @@ fun FindUserCard(
     Card(
         modifier = modifier
             .padding(20.dp)
-            .fillMaxWidth()
+            .fillMaxWidth(),
+        colors = CardDefaults.cardColors(containerColor = MaterialTheme.colorScheme.surfaceContainerLow)
     ) {
         Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(vertical = 20.dp),
+                .padding(vertical = 32.dp),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             var imageUrl by rememberSaveable { mutableStateOf("") }
             LaunchedEffect(userData) { imageUrl = getImageFromFireStore(userData.picture).first() }
             GlideImage(
-                imageModel = { imageUrl.ifBlank { R.drawable.icons8__ } },
+                imageModel = { imageUrl.ifBlank { R.drawable.baseline_person_24 } },
                 modifier = Modifier
-                    .sizeIn(
-                        maxWidth = 80.dp,
-                        maxHeight = 80.dp
+                    .size(
+                        width = 80.dp,
+                        height = 80.dp
                     )
-                    .clip(RoundedCornerShape(20.dp)),
+                    .clip(RoundedCornerShape(20.dp))
+                    .background(color = colorResource(R.color.avatar_background)),
                 component = rememberImageComponent {
                     +ShimmerPlugin(
                         Shimmer.Flash(
@@ -259,11 +267,22 @@ fun FindUserCard(
                         )
                     )
                 },
-                previewPlaceholder = painterResource(id = R.drawable.icons8__)
+                previewPlaceholder = painterResource(id = R.drawable.baseline_person_24)
             )
+            Spacer(modifier = Modifier.height(12.dp))
             Text(text = userData.name)
-            TextButton(onClick = onFindComplete) {
-                Text(text = stringResource(R.string.find_user_screen_add_button_text))
+            Spacer(modifier = Modifier.height(20.dp))
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                OutlinedButton(onClick = {}) {
+                    Text("친구 차단")
+                }
+                Spacer(modifier = Modifier.width(20.dp))
+                Button(onClick = onFindComplete) {
+                    Text(text = stringResource(R.string.find_user_screen_add_button_text))
+                }
             }
         }
     }
