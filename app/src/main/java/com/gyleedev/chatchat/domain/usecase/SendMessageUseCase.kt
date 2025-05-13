49148d8288b6227e3d88a3f5dd342f7d293d7eb6
ThinkGYLee <ntnpholicfp@gmail.com>
2025-05-13 19:44:58 +0900

    - SendMessageUseCase.kt - useCase 들을 받아서 보내는 로직을 구현해 놨던 부분 제거 - repository.sendMessage() 와 연결

--- app/src/main/java/com/gyleedev/chatchat/domain/usecase/SendMessageUseCase.kt
+++ app/src/main/java/com/gyleedev/chatchat/domain/usecase/SendMessageUseCase.kt
@@ -1,43 +1,13 @@
 package com.gyleedev.chatchat.domain.usecase
 
+import com.gyleedev.chatchat.data.repository.MessageRepository
 import com.gyleedev.chatchat.domain.MessageData
-import com.gyleedev.chatchat.domain.MessageSendState
-import com.gyleedev.chatchat.domain.MessageType
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class SendMessageUseCase @Inject constructor(
-    private val insertMessageToLocalUseCase: InsertMessageToLocalUseCase,
-    private val sendMessageToRemoteUseCase: SendMessageToRemoteUseCase,
-    private val updateMessageStateUseCase: UpdateMessageStateUseCase,
-    private val uploadImageToRemoteUseCase: UploadImageToRemoteUseCase
+    private val repository: MessageRepository
 ) {
     suspend operator fun invoke(messageData: MessageData, rid: Long, networkState: Boolean) {
-        withContext(Dispatchers.IO) {
-            val message = if (messageData.type == MessageType.Photo) {
-                messageData.copy(
-                    comment = uploadImageToRemoteUseCase(messageData.comment).first()
-                )
-            } else {
-                messageData
-            }
-            val messageId = insertMessageToLocalUseCase(message, rid)
-            if (networkState) {
-                val request = try {
-                    sendMessageToRemoteUseCase(message).first()
-                } catch (e: Throwable) {
-                    println(e)
-                }
-                if (request is MessageSendState) {
-                    val updateMessage = message.copy(messageSendState = request)
-                    updateMessageStateUseCase(messageId, rid, updateMessage)
-                }
-            } else {
-                val updateMessage = message.copy(messageSendState = MessageSendState.FAIL)
-                updateMessageStateUseCase(messageId, rid, updateMessage)
-            }
-        }
+        repository.sendMessage(messageData, rid, networkState)
     }
 }
