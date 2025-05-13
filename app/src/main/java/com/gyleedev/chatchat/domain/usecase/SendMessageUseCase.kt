cad9246e83ea9473919c67963180ec60deb106f9
ThinkGYLee <ntnpholicfp@gmail.com>
2025-05-13 20:15:41 +0900

    - SendMessageUseCase.kt - UI potential Lock 이슈로 WithContext로 감싸서 해결

--- app/src/main/java/com/gyleedev/chatchat/domain/usecase/SendMessageUseCase.kt
+++ app/src/main/java/com/gyleedev/chatchat/domain/usecase/SendMessageUseCase.kt
@@ -2,12 +2,16 @@ package com.gyleedev.chatchat.domain.usecase
 
 import com.gyleedev.chatchat.data.repository.MessageRepository
 import com.gyleedev.chatchat.domain.MessageData
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class SendMessageUseCase @Inject constructor(
     private val repository: MessageRepository
 ) {
     suspend operator fun invoke(messageData: MessageData, rid: Long, networkState: Boolean) {
-        repository.sendMessage(messageData, rid, networkState)
+        withContext(Dispatchers.IO) {
+            repository.sendMessage(messageData, rid, networkState)
+        }
     }
 }
