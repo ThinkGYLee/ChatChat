ecbba7071caa8267f8feb1e89b07fdea30186e5a
ThinkGYLee <ntnpholicfp@gmail.com>
2025-05-14 18:07:38 +0900

    - UriMetaData - @Parcelize, Parcelable 추가

--- app/src/main/java/com/gyleedev/chatchat/domain/UrlMetaData.kt
+++ app/src/main/java/com/gyleedev/chatchat/domain/UrlMetaData.kt
@@ -1,10 +1,15 @@
 package com.gyleedev.chatchat.domain
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
 // 기본값 설정 안해주면 crash남
+
+@Parcelize
 data class UrlMetaData(
-    var title: String? = null,
-    var name: String? = null,
-    var description: String? = null,
-    var url: String? = null,
-    var imageUrl: String? = null
-)
+    var title: String = "",
+    var name: String = "",
+    var description: String = "",
+    var url: String = "",
+    var imageUrl: String = ""
+) : Parcelable
