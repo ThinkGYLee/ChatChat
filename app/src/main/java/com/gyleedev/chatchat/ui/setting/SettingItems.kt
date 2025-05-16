f4cd1e3e753676dea75bda7aab8aef00ea2c24d7
ThinkGYLee <ntnpholicfp@gmail.com>
2025-05-16 21:16:03 +0900

    - Const 에서 MANAGEHIDE, MANAGEBLOCKED 항목 수정

---
+++ app/src/main/java/com/gyleedev/chatchat/ui/setting/SettingItems.kt
@@ -0,0 +1,15 @@
+package com.gyleedev.chatchat.ui.setting
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+sealed interface SettingItems {
+
+    data class Header(
+        val title: Int
+    ) : SettingItems
+
+    data class Item(
+        val title: Int,
+        val icon: ImageVector
+    ) : SettingItems
+}
