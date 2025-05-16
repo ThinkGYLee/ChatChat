c8a7ef7200d2d0cbb6b9e88efe72b5ad92e1746e
ThinkGYLee <ntnpholicfp@gmail.com>
2025-05-17 03:52:47 +0900

    - ChatRoomScreen.kt - 유니버설바 백그라운드 컬러 수정 - 유니버설바 바텀패딩 추가 - 유니버설바 포커스 지우는 버튼 arrowBack 에서 arrowForward 로 변경 - CommentBar 데코레이터의 박스 백그라운드 컬러 변경

--- app/src/main/java/com/gyleedev/chatchat/ui/chatroom/ChatRoomScreen.kt
+++ app/src/main/java/com/gyleedev/chatchat/ui/chatroom/ChatRoomScreen.kt
@@ -40,9 +40,9 @@ import androidx.compose.foundation.text.input.delete
 import androidx.compose.foundation.text.input.rememberTextFieldState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.ArrowForwardIos
 import androidx.compose.material.icons.automirrored.filled.Send
 import androidx.compose.material.icons.automirrored.outlined.Send
-import androidx.compose.material.icons.filled.ArrowBackIosNew
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Image
 import androidx.compose.material.icons.outlined.Block
@@ -706,7 +706,8 @@ fun UniversalBar(
         Column(
             modifier = modifier
                 .fillMaxWidth()
-                .background(color = BottomAppBarDefaults.containerColor)
+                .background(color = MaterialTheme.colorScheme.surfaceContainerLowest)
+                .padding(bottom = 4.dp)
                 .windowInsetsPadding(BottomAppBarDefaults.windowInsets)
                 .imePadding()
         ) {
@@ -734,7 +735,7 @@ fun UniversalBar(
                         onClick = { focusManager.clearFocus() }
                     ) {
                         Icon(
-                            imageVector = Icons.Default.ArrowBackIosNew,
+                            imageVector = Icons.AutoMirrored.Filled.ArrowForwardIos,
                             contentDescription = null
                         )
                     }
@@ -831,7 +832,7 @@ fun CommentBottomBar(
         decorator = { innerTextField ->
             Box(
                 modifier = Modifier.background(
-                    MaterialTheme.colorScheme.surfaceContainerLow,
+                    MaterialTheme.colorScheme.surfaceContainer,
                     shape = RoundedCornerShape(20.dp)
                 )
             ) {
