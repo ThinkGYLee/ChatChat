10d2ec360cf0b12bd5dae6044b139e7b89824c59
ThinkGYLee <ntnpholicfp@gmail.com>
2025-05-29 00:04:31 +0900

    - HiltKotlin.kt - extension 제거

--- build-logic/src/main/java/com/gyleedev/buildlogic/HiltKotlin.kt
+++ build-logic/src/main/java/com/gyleedev/buildlogic/HiltKotlin.kt
@@ -1,16 +1,12 @@
-package com.gyleedev.build_logic
+package com.gyleedev.buildlogic
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import org.gradle.api.artifacts.VersionCatalog
-import org.gradle.api.artifacts.VersionCatalogsExtension
-import org.gradle.api.plugins.ExtensionContainer
 import org.gradle.kotlin.dsl.dependencies
 
 internal fun Project.configureHiltKotlin() {
     with(pluginManager) {
         apply("com.google.devtools.ksp")
-        apply("com.google.dagger.hilt.android")
     }
 
     val libs = extensions.libs
@@ -29,5 +25,3 @@ internal class HiltKotlinPlugin : Plugin<Project> {
     }
 }
 
-internal val ExtensionContainer.libs: VersionCatalog
-    get() = getByType(VersionCatalogsExtension::class.java).named("libs")
