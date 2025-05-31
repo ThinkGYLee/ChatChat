77313f6b055172e21e87dabc537188c906023ad0
ThinkGYLee <ntnpholicfp@gmail.com>
2025-05-31 22:56:36 +0900

    - GetChatRoomDataWithRelatedUserUseCase.kt - GetChatRoomListUseCase.kt 에서 명칭 변경 - 유저정보와 챗룸데이터 함께 가져오는 로직을 유스케이스에서 가져와서 합치도록 구현

---
+++ app/src/main/java/com/gyleedev/chatchat/domain/usecase/GetChatRoomDataWithRelatedUserUseCase.kt
@@ -0,0 +1,39 @@
+package com.gyleedev.chatchat.domain.usecase
+
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import androidx.paging.map
+import com.gyleedev.chatchat.data.database.entity.toModel
+import com.gyleedev.chatchat.data.repository.ChatRoomRepository
+import com.gyleedev.chatchat.data.repository.UserRepository
+import com.gyleedev.chatchat.domain.ChatRoomDataWithRelatedUsers
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+class GetChatRoomDataWithRelatedUserUseCase @Inject constructor(
+    private val chatRoomRepository: ChatRoomRepository,
+    private val userRepository: UserRepository
+) {
+    suspend operator fun invoke(): Flow<PagingData<ChatRoomDataWithRelatedUsers>> {
+        return withContext(Dispatchers.IO) {
+            val relatedUsers = userRepository.getRelatedUsersForChatRoomList()
+            Pager(
+                config = PagingConfig(pageSize = 10, enablePlaceholders = false),
+                pagingSourceFactory = {
+                    chatRoomRepository.getChatRoomListWithPaging()
+                }
+            ).flow.map { value ->
+                value.map { chatRoom ->
+                    ChatRoomDataWithRelatedUsers(
+                        chatRoomLocalData = chatRoom.toModel(),
+                        relatedUserLocalData = relatedUsers.find { chatRoom.receiver == it.uid }!!
+                    )
+                }
+            }
+        }
+    }
+}
