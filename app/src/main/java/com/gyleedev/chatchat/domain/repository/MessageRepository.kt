c2257f61d2b9134a1b924b097ae30001cfce9eed
ThinkGYLee <ntnpholicfp@gmail.com>
2025-06-01 20:21:21 +0900

    - MessageRepository Domain 레이어로 분리

---
+++ app/src/main/java/com/gyleedev/chatchat/domain/repository/MessageRepository.kt
@@ -0,0 +1,36 @@
+package com.gyleedev.chatchat.domain.repository
+
+import androidx.paging.PagingData
+import com.gyleedev.chatchat.data.database.entity.MessageEntity
+import com.gyleedev.chatchat.domain.model.ChatRoomLocalData
+import com.gyleedev.chatchat.domain.model.MessageData
+import com.gyleedev.chatchat.domain.model.MessageSendState
+import com.gyleedev.chatchat.domain.model.ProcessResult
+import com.gyleedev.chatchat.domain.model.UserRelationState
+import kotlinx.coroutines.flow.Flow
+
+interface MessageRepository {
+    suspend fun insertMessageToLocal(message: MessageData, roomId: Long): Long
+    fun insertMessageToRemote(message: MessageData): Flow<MessageSendState>
+    suspend fun updateMessageState(messageId: Long, roomId: Long, message: MessageData)
+
+    fun getMessageListener(
+        chatRoom: ChatRoomLocalData,
+        userRelationState: UserRelationState
+    ): Flow<MessageData?>
+
+    suspend fun getLastMessage(chatRoomId: String): MessageEntity?
+
+    fun getMessagesFromLocal(rid: String): Flow<PagingData<MessageData>>
+
+    fun getMessage(message: MessageData): Flow<MessageEntity>
+
+    suspend fun deleteLocalMessage(messageId: Long)
+
+    suspend fun resetMessageData()
+
+    suspend fun deleteRemoteMessage(message: MessageData): Flow<ProcessResult>
+
+    suspend fun deleteMessageRequest(message: MessageData): Flow<ProcessResult>
+    suspend fun sendMessage(messageData: MessageData, rid: Long, networkState: Boolean)
+}
