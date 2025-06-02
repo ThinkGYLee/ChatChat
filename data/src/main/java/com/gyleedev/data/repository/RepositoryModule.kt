e42f43b7276a58dcbad9c60042c56095dbd962db
ThinkGYLee <ntnpholicfp@gmail.com>
2025-06-02 15:11:38 +0900

    - RepositoryModule.kt - SettingRepository 관련 binds 함수 추가

--- data/src/main/java/com/gyleedev/data/repository/RepositoryModule.kt
+++ data/src/main/java/com/gyleedev/data/repository/RepositoryModule.kt
@@ -2,6 +2,7 @@ package com.gyleedev.data.repository
 
 import com.gyleedev.domain.repository.ChatRoomRepository
 import com.gyleedev.domain.repository.MessageRepository
+import com.gyleedev.domain.repository.SettingRepository
 import com.gyleedev.domain.repository.UserRepository
 import dagger.Binds
 import dagger.Module
@@ -23,4 +24,8 @@ abstract class RepositoryModule {
     @Binds
     @Singleton
     abstract fun bindsChatRoomRepository(impl: ChatRoomRepositoryImpl): ChatRoomRepository
+
+    @Binds
+    @Singleton
+    abstract fun bindsSettingRepository(impl: SettingRepositoryImpl): SettingRepository
 }
