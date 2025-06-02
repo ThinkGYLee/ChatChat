82d1b0dadcea29e468362f29379cc3c1c06bb91c
ThinkGYLee <ntnpholicfp@gmail.com>
2025-06-02 16:54:48 +0900

    - Feature 모듈 생성

---
+++ feature/build.gradle.kts
@@ -0,0 +1,29 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    id("chat.android.kotlin")
+    id("chat.android.hilt")
+    id("chat.kotlin.hilt")
+    id("chat.android.ui")
+}
+
+android {
+    namespace = "com.gyleedev.feature"
+
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_17)
+        }
+    }
+
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+    implementation(project(":domain"))
+    implementation(project(":core"))
+    implementation(project(":util"))
+    implementation(libs.androidx.browser)
+}
