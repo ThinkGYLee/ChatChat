b473bb92aeba018c609fb49fd7966eae69009a0b
ThinkGYLee <ntnpholicfp@gmail.com>
2025-06-02 17:03:28 +0900

    - app 모듈에 있는 ui 관련 파일 제거

--- app/src/main/java/com/gyleedev/chatchat/ui/finduser/FindProcessState.kt
+++
@@ -1,8 +0,0 @@
-package com.gyleedev.chatchat.ui.finduser
-
-enum class FindProcessState {
-    Complete,
-    SearchFailure,
-    AddFailure,
-    BlockFailure
-}
--- app/src/main/java/com/gyleedev/chatchat/ui/setting/SettingEvent.kt
+++
@@ -1,9 +0,0 @@
-package com.gyleedev.chatchat.ui.setting
-
-enum class SettingEvent {
-    LANGUAGE,
-    ACCOUNT,
-    MYINFORMATION,
-    THEME,
-    CHAT
-}
