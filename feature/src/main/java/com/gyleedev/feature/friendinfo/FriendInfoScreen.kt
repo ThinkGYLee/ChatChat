bd3b3beae94aef8405e15f0983adc6213c7f61ef
ThinkGYLee <ntnpholicfp@gmail.com>
2025-06-03 17:14:56 +0900

    - FriendInfoScreen.kt - 채팅 아이콘에 clip 적용

--- feature/src/main/java/com/gyleedev/feature/friendinfo/FriendInfoScreen.kt
+++ feature/src/main/java/com/gyleedev/feature/friendinfo/FriendInfoScreen.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -27,6 +28,7 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
@@ -37,6 +39,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
@@ -152,17 +155,25 @@ fun FriendInfoScreen(
             Text(text = friendData?.status ?: stringResource(R.string.blank_text))
             Spacer(modifier = Modifier.height(60.dp))
             Row(modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Center) {
-                Column(
-                    modifier = Modifier.clickable { friendData?.let { onChatRoomClick(it.uid) } },
-                    horizontalAlignment = Alignment.CenterHorizontally
+                Box(
+                    modifier = Modifier
+                        .clip(CircleShape)
+                        .clickable { friendData?.let { onChatRoomClick(it.uid) } },
+
                 ) {
-                    Icon(
-                        imageVector = Icons.Outlined.Email,
-                        contentDescription = stringResource(R.string.message_button_description)
-                    )
-                    Spacer(modifier = Modifier.height(8.dp))
-                    Text(text = stringResource(R.string.message_button_message))
+                    Column(
+                        modifier = Modifier.padding(10.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Icon(
+                            imageVector = Icons.Outlined.Email,
+                            contentDescription = stringResource(R.string.message_button_description)
+                        )
+                        Spacer(modifier = Modifier.height(8.dp))
+                        Text(text = stringResource(R.string.message_button_message))
+                    }
                 }
+
             }
             Spacer(modifier = Modifier.height(72.dp))
         }
