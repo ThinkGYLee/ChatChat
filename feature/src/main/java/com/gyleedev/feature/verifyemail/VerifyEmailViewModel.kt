7209a0aa31d0e5509681d50bc767178cc0aab989
ThinkGYLee <ntnpholicfp@gmail.com>
2025-06-07 23:59:11 +0900

    - VerifyEmailViewModel.kt - 의존성 추가 - verifyEmailRequestUseCase - getVerifiedStateUseCase - setVerifiedStateUseCase - checkVerifiedUseCase - updateMyUserInformationUseCase - verifyCheckResult SharedFlow 항목 추가 - VerifyEmailUiState 에 verifiedState 항목 추가된 부분 반영해서 수정 - verifyRequest 구현 - checkVerified 구현

--- feature/src/main/java/com/gyleedev/feature/verifyemail/VerifyEmailViewModel.kt
+++ feature/src/main/java/com/gyleedev/feature/verifyemail/VerifyEmailViewModel.kt
@@ -2,20 +2,32 @@ package com.gyleedev.feature.verifyemail
 
 import androidx.lifecycle.viewModelScope
 import com.gyleedev.core.BaseViewModel
+import com.gyleedev.domain.model.VerifiedState
 import com.gyleedev.domain.usecase.CancelSigninUseCase
+import com.gyleedev.domain.usecase.CheckVerifiedUseCase
 import com.gyleedev.domain.usecase.GetMyDataFromPreferenceUseCase
+import com.gyleedev.domain.usecase.GetVerifiedStateUseCase
+import com.gyleedev.domain.usecase.SetVerifiedStateUseCase
+import com.gyleedev.domain.usecase.UpdateMyInfoUseCase
+import com.gyleedev.domain.usecase.VerifyEmailRequestUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class VerifyEmailViewModel @Inject constructor(
     private val getMyDataFromPreferenceUseCase: GetMyDataFromPreferenceUseCase,
-    private val cancelSigninUseCase: CancelSigninUseCase
+    private val verifyEmailRequestUseCase: VerifyEmailRequestUseCase,
+    private val getVerifiedStateUseCase: GetVerifiedStateUseCase,
+    private val setVerifiedStateUseCase: SetVerifiedStateUseCase,
+    private val checkVerifiedUseCase: CheckVerifiedUseCase,
+    private val cancelSigninUseCase: CancelSigninUseCase,
+    private val updateMyUserInformationUseCase: UpdateMyInfoUseCase
 ) : BaseViewModel() {
 
     private val _uiState = MutableStateFlow<VerifyEmailUiState>(VerifyEmailUiState.Loading)
@@ -24,12 +36,15 @@ class VerifyEmailViewModel @Inject constructor(
     private val _cancelSigninResult = MutableSharedFlow<Boolean>()
     val cancelSigninResult: SharedFlow<Boolean> = _cancelSigninResult
 
+    private val _verifyCheckResult = MutableSharedFlow<Boolean>()
+    val verifyCheckResult: SharedFlow<Boolean> = _verifyCheckResult
 
     init {
         viewModelScope.launch {
             _uiState.emit(
                 VerifyEmailUiState.Success(
-                    userData = getMyDataFromPreferenceUseCase()
+                    userData = getMyDataFromPreferenceUseCase(),
+                    verifiedState = getVerifiedStateUseCase()
                 )
             )
         }
@@ -39,6 +54,42 @@ class VerifyEmailViewModel @Inject constructor(
         viewModelScope.launch {
             val result = cancelSigninUseCase()
             _cancelSigninResult.emit(result)
+        }
+    }
+
+    fun verifyRequest() {
+        viewModelScope.launch {
+            val result = verifyEmailRequestUseCase()
+            if (result) {
+                setVerifiedStateUseCase(VerifiedState.INPROGRESS)
+                _uiState.emit(
+                    VerifyEmailUiState.Success(
+                        userData = (_uiState.value as VerifyEmailUiState.Success).userData,
+                        verifiedState = VerifiedState.INPROGRESS
+                    )
+                )
+            } else {
+                _verifyCheckResult.emit(false)
+            }
+        }
+    }
+
+    fun checkVerified() {
+        viewModelScope.launch {
+            val result = checkVerifiedUseCase()
+            if (result) {
+                val updateResult = updateMyUserInformationUseCase(
+                    (_uiState.value as VerifyEmailUiState.Success).userData.copy(verified = true)
+                ).first()
+                if (updateResult) {
+                    setVerifiedStateUseCase(VerifiedState.VERIFIED)
+                    _verifyCheckResult.emit(true)
+                } else {
+                    _verifyCheckResult.emit(false)
+                }
+            } else {
+                _verifyCheckResult.emit(false)
+            }
         }
     }
 }
