e47d0f7c2e48ca6e0116d6379207ddce7004ce51
ThinkGYLee <ntnpholicfp@gmail.com>
2025-06-07 23:56:48 +0900

    - CheckVerifiedUseCase 추가

---
+++ domain/src/main/java/com/gyleedev/domain/usecase/CheckVerifiedUseCase.kt
@@ -0,0 +1,13 @@
+package com.gyleedev.domain.usecase
+
+import com.gyleedev.domain.repository.UserRepository
+import kotlinx.coroutines.flow.first
+import javax.inject.Inject
+
+class CheckVerifiedUseCase @Inject constructor(
+    private val repository: UserRepository
+) {
+    suspend operator fun invoke(): Boolean {
+        return repository.checkUserVerified().first()
+    }
+}
