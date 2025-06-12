402babcc3c88fc74b6d6733b022d75e4291d3300
ThinkGYLee <ntnpholicfp@gmail.com>
2025-06-12 22:55:31 +0900

    - RetrieveAndInsertChatRoomDataFromRemoteUseCase.kt - 로직 변경, 리턴타입 변경

--- domain/src/main/java/com/gyleedev/domain/usecase/RetrieveAndInsertChatRoomDataFromRemoteUseCase.kt
+++ domain/src/main/java/com/gyleedev/domain/usecase/RetrieveAndInsertChatRoomDataFromRemoteUseCase.kt
@@ -1,27 +1,27 @@
 package com.gyleedev.domain.usecase
 
-import com.gyleedev.domain.model.ChatRoomData
+import com.gyleedev.domain.model.GetChatRoomResult
 import com.gyleedev.domain.model.RelatedUserLocalData
 import com.gyleedev.domain.repository.ChatRoomRepository
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.firstOrNull
-import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class RetrieveAndInsertChatRoomDataFromRemoteUseCase @Inject constructor(
     private val repository: ChatRoomRepository
 ) {
-    suspend operator fun invoke(relatedUserLocalData: RelatedUserLocalData): ChatRoomData? {
+    suspend operator fun invoke(relatedUserLocalData: RelatedUserLocalData): GetChatRoomResult {
         return withContext(Dispatchers.IO) {
-            var result: ChatRoomData?
-            runBlocking {
-                result = repository.getChatRoomFromRemote(relatedUserLocalData).firstOrNull()
-            }
+            var result = repository.getChatRoomFromRemote(relatedUserLocalData).firstOrNull()
             if (result != null) {
                 repository.insertChatRoomToLocal(relatedUserLocalData, result)
+                GetChatRoomResult.Success(
+                    chatRoom = result
+                )
+            } else {
+                GetChatRoomResult.Failure
             }
-            result
         }
     }
 }
