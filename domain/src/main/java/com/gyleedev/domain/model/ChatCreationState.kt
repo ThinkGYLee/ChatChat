5cf1e8d48403d8e15e437d861d437c616491f1e1
ThinkGYLee <ntnpholicfp@gmail.com>
2025-06-21 23:32:28 +0900

    - ChatCreationState.kt - GetRemoteData 추가

--- domain/src/main/java/com/gyleedev/domain/model/ChatCreationState.kt
+++ domain/src/main/java/com/gyleedev/domain/model/ChatCreationState.kt
@@ -3,6 +3,7 @@ package com.gyleedev.domain.model
 sealed interface ChatCreationState {
     data object CheckAndGetDataFromLocal : ChatCreationState
     data object CheckingRemoteChatRoomExists : ChatCreationState
+    data object GetRemoteData: ChatCreationState
     data object CheckingMyDataExists : ChatCreationState
     data object CheckingFriendDataExists : ChatCreationState
     data object CreatingRemoteChatRoom : ChatCreationState
