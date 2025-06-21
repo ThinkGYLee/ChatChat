a784296ae15acb8d10533e54671765a4f0a8bd0b
ThinkGYLee <ntnpholicfp@gmail.com>
2025-06-22 01:09:43 +0900

    - GetChatRoomState.kt - 채팅방을 만드는 기능이 아니기 때문에 명칭 변경

---
+++ domain/src/main/java/com/gyleedev/domain/model/GetChatRoomState.kt
@@ -0,0 +1,16 @@
+package com.gyleedev.domain.model
+
+sealed interface GetChatRoomState {
+    data object CheckAndGetDataFromLocal : GetChatRoomState
+    data object CheckingRemoteGetChatRoomExists : GetChatRoomState
+    data object GetRemoteData : GetChatRoomState
+    data object CheckingMyDataExists : GetChatRoomState
+    data object CheckingFriendDataExists : GetChatRoomState
+    data object CreatingRemoteGetChatRoom : GetChatRoomState
+    data object UpdateMyData : GetChatRoomState
+    data object UpdateFriendData : GetChatRoomState
+    data object SavingGetChatRoomToLocal : GetChatRoomState
+    data class Success(
+        val data: ChatRoomLocalData
+    ) : GetChatRoomState
+}
