0396587ad9059d51d3a7bbaa5ec5249e479b5c9e
ThinkGYLee <ntnpholicfp@gmail.com>
2025-06-22 03:05:47 +0900

    - GetChatRoomUseCase.kt - suspend fun 으로 수정 - withContext 추가

--- domain/src/main/java/com/gyleedev/domain/usecase/GetChatRoomUseCase.kt
+++ domain/src/main/java/com/gyleedev/domain/usecase/GetChatRoomUseCase.kt
@@ -1,15 +1,21 @@
 package com.gyleedev.domain.usecase
 
-import com.gyleedev.domain.model.ChatCreationState
+import com.gyleedev.domain.model.GetChatRoomState
 import com.gyleedev.domain.model.RelatedUserLocalData
 import com.gyleedev.domain.repository.ChatRoomRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class GetChatRoomUseCase @Inject constructor(
     private val repository: ChatRoomRepository
 ) {
-    operator fun invoke(
+    suspend operator fun invoke(
         user: RelatedUserLocalData,
-        chatCreationState: ChatCreationState = ChatCreationState.CheckAndGetDataFromLocal
-    ) = repository.getChatRoom(user, chatCreationState)
+        getChatRoomState: GetChatRoomState = GetChatRoomState.CheckAndGetDataFromLocal
+    ): GetChatRoomState {
+        return withContext(Dispatchers.IO) {
+            repository.getChatRoom(user, getChatRoomState)
+        }
+    }
 }
