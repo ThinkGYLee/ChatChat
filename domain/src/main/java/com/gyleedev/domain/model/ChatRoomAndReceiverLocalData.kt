b4c1c710fb244d90c69853aafd93c771db5f7465
ThinkGYLee <ntnpholicfp@gmail.com>
2025-06-25 22:39:08 +0900

    - ChatRoomAndReceiverLocalData 추가

---
+++ domain/src/main/java/com/gyleedev/domain/model/ChatRoomAndReceiverLocalData.kt
@@ -0,0 +1,10 @@
+package com.gyleedev.domain.model
+
+data class ChatRoomAndReceiverLocalData(
+    val id: Long = 0,
+    val rid: String = "",
+    val lastMessage: String = "",
+    val receivers: List<String> = listOf<String>()
+)
+// 챗룸정보 테이블 -> 챗룸id를 키값으로
+// 유저별 챗룸 정보 테이블 -> 유저 아이디를 키값으로 사용 내용에 상대Uid, 챗방Uid 저장
