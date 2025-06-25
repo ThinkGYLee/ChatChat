ebeb0d7a815af6a86c217201a69067c200beb307
ThinkGYLee <ntnpholicfp@gmail.com>
2025-06-26 00:36:19 +0900

    - GetChatRoomState.kt - CreatingRemoteChatRoomData 로 이름 변경 - 이하의 항목 추가 - CheckingMyReceiverExists - CheckingFriendReceiverExists - InsertMyDataToReceiver - InsertFriendDataToReceiver

--- domain/src/main/java/com/gyleedev/domain/model/GetChatRoomState.kt
+++ domain/src/main/java/com/gyleedev/domain/model/GetChatRoomState.kt
@@ -7,7 +7,11 @@ sealed interface GetChatRoomState {
     data object GetRemoteData : GetChatRoomState
     data object CheckingMyDataExists : GetChatRoomState
     data object CheckingFriendDataExists : GetChatRoomState
-    data object CreatingRemoteGetChatRoom : GetChatRoomState
+    data object CreatingRemoteChatRoomData : GetChatRoomState
+    data object CheckingMyReceiverExists : GetChatRoomState
+    data object CheckingFriendReceiverExists : GetChatRoomState
+    data object InsertMyDataToReceiver : GetChatRoomState
+    data object InsertFriendDataToReceiver : GetChatRoomState
     data object UpdateMyData : GetChatRoomState
     data object UpdateFriendData : GetChatRoomState
     data object SavingGetChatRoomToLocal : GetChatRoomState
