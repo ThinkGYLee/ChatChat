3f8e4eaf38a27851fe8154632a6d9d07fe260311
ThinkGYLee <ntnpholicfp@gmail.com>
2025-06-28 05:03:47 +0900

    - ChatRoomAndReceiverDao.kt - getChatRoomAndReceiverByUid 인수 변경 - getChatRoomsWithPaging 리턴타입 변경 - 안쓰는 함수 제거

--- data/src/main/java/com/gyleedev/data/database/dao/ChatRoomAndReceiverDao.kt
+++ data/src/main/java/com/gyleedev/data/database/dao/ChatRoomAndReceiverDao.kt
@@ -12,24 +12,25 @@ import com.gyleedev.data.database.entity.ChatRoomEntity
 @Dao
 interface ChatRoomAndReceiverDao {
 
-    @Insert
-    fun insertChatRoom(chatRoom: ChatRoomEntity): Long
-
     @Transaction
-    @Query("""
+    @Query(
+        """
         SELECT * FROM chatroom
-        WHERE id IN (
+        WHERE isGroup = :isGroup AND id IN (
             SELECT chatroom_entity_id FROM receiver WHERE receiver = :receiver
         )
-    """)
-    fun getChatRoomByUid(receiver: String): ChatRoomAndReceiverEntity?
+    """
+    )
+    fun getChatRoomAndReceiverByUid(
+        receiver: String,
+        isGroup: Boolean = false
+    ): ChatRoomAndReceiverEntity?
 
-    @Update
-    fun updateChatRoom(chatRoom: ChatRoomEntity)
+    @Transaction
+    @Query("SELECT * FROM chatroom WHERE rid = :rid")
+    fun getChatRoomAndReceiverByRid(rid: String): ChatRoomAndReceiverEntity?
 
+    @Transaction
     @Query("SELECT * FROM chatroom")
-    fun getChatRoomsWithPaging(): PagingSource<Int, ChatRoomEntity>
-
-    @Query("DELETE FROM chatroom")
-    suspend fun resetChatRoomDatabase()
+    fun getChatRoomsWithPaging(): PagingSource<Int, ChatRoomAndReceiverEntity>
 }
