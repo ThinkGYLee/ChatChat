a0172ec207818132e676fcf8f366abac3ebb89a6
ThinkGYLee <ntnpholicfp@gmail.com>
2025-06-28 05:14:22 +0900

    - ChatRoomDataWithAllRelatedUsersAndMessage.kt - relatedUserLocalData 의 타입을 리스트타입으로 수정 - chatRoomLocalData 의 타입을 ChatRoomAndReceiverLocalData 로 변경

--- domain/src/main/java/com/gyleedev/domain/model/ChatRoomDataWithAllRelatedUsersAndMessage.kt
+++ domain/src/main/java/com/gyleedev/domain/model/ChatRoomDataWithAllRelatedUsersAndMessage.kt
@@ -1,8 +1,8 @@
 package com.gyleedev.domain.model
 
 data class ChatRoomDataWithAllRelatedUsersAndMessage(
-    val chatRoomLocalData: ChatRoomLocalData,
-    val relatedUserLocalData: RelatedUserLocalData,
+    val chatRoomAndReceiverLocalData: ChatRoomAndReceiverLocalData,
+    val receiversInfo: List<RelatedUserLocalData>,
     val lastMessageData: MessageData
 )
 // view에서 lastMessage 매핑하는 용
