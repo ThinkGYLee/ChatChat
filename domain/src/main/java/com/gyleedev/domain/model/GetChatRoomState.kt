a80b5264d051be9dd49853c68c02e635bab1ccb8
ThinkGYLee <ntnpholicfp@gmail.com>
2025-06-28 04:58:26 +0900

    - GetChatRoomState.kt - 공통, uid에서 사용하는 부분, rid에서 사용하는 부분 구분 - GetRemoteReceivers, CompareAndInsertReceiversToLocal, ReturnChatRoom 추가

--- domain/src/main/java/com/gyleedev/domain/model/GetChatRoomState.kt
+++ domain/src/main/java/com/gyleedev/domain/model/GetChatRoomState.kt
@@ -1,8 +1,12 @@
 package com.gyleedev.domain.model
 
 sealed interface GetChatRoomState {
+
+    // Common
     data object None : GetChatRoomState
     data object CheckAndGetDataFromLocal : GetChatRoomState
+
+    // ByUid
     data object CheckingRemoteGetChatRoomExists : GetChatRoomState
     data object GetRemoteData : GetChatRoomState
     data object CheckingMyDataExists : GetChatRoomState
@@ -15,6 +19,12 @@ sealed interface GetChatRoomState {
     data object UpdateMyData : GetChatRoomState
     data object UpdateFriendData : GetChatRoomState
     data object SavingGetChatRoomToLocal : GetChatRoomState
+
+    // ByRid
+    data object GetRemoteReceivers : GetChatRoomState
+    data object CompareAndInsertReceiversToLocal : GetChatRoomState
+    data object ReturnChatRoom : GetChatRoomState
+
     data class Success(
         val data: ChatRoomAndReceiverLocalData
     ) : GetChatRoomState
