d806718af1063a53d39c28331122d220a0c547da
ThinkGYLee <ntnpholicfp@gmail.com>
2025-06-28 05:17:37 +0900

    - GetRelatedUserDataOfParticipantsByUidUseCase 추가

---
+++ domain/src/main/java/com/gyleedev/domain/usecase/GetRelatedUserDataOfParticipantsByUidUseCase.kt
@@ -0,0 +1,12 @@
+package com.gyleedev.domain.usecase
+
+import com.gyleedev.domain.repository.UserRepository
+import javax.inject.Inject
+
+class GetRelatedUserDataOfParticipantsByUidUseCase @Inject constructor(
+    private val repository: UserRepository
+) {
+    suspend operator fun invoke(
+        uidList: List<String>
+    ) = repository.getUsersByUid(uidList)
+}
