d0bc64d0756067202f58be00857a8839467e1f0b
ThinkGYLee <ntnpholicfp@gmail.com>
2025-06-28 05:00:16 +0900

    - ReceiverDao.kt - insertReceivers 추가 - getReceiverByUid,updateReceiver 제거

--- data/src/main/java/com/gyleedev/data/database/dao/ReceiverDao.kt
+++ data/src/main/java/com/gyleedev/data/database/dao/ReceiverDao.kt
@@ -1,11 +1,7 @@
 package com.gyleedev.data.database.dao
 
-import androidx.paging.PagingSource
 import androidx.room.Dao
 import androidx.room.Insert
-import androidx.room.Query
-import androidx.room.Update
-import com.gyleedev.data.database.entity.ChatRoomEntity
 import com.gyleedev.data.database.entity.ReceiverEntity
 
 @Dao
@@ -14,9 +10,6 @@ interface ReceiverDao {
     @Insert
     fun insertReceiver(receiver: ReceiverEntity): Long
 
-    @Query("SELECT * FROM receiver WHERE receiver = :receiver")
-    fun getReceiverByUid(receiver: String): ReceiverEntity?
-
-    @Update
-    fun updateReceiver(receiver: ReceiverEntity)
+    @Insert
+    fun insertReceivers(list: List<ReceiverEntity>)
 }
