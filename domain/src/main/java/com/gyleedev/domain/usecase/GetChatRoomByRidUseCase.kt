0cd34582cecc0c7fdaace0cae8beb9e973c30dc6
ThinkGYLee <ntnpholicfp@gmail.com>
2025-06-28 05:17:48 +0900

    - GetChatRoomByRidUseCase 추가

---
+++ domain/src/main/java/com/gyleedev/domain/usecase/GetChatRoomByRidUseCase.kt
@@ -0,0 +1,20 @@
+package com.gyleedev.domain.usecase
+
+import com.gyleedev.domain.model.GetChatRoomState
+import com.gyleedev.domain.repository.ChatRoomRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import javax.inject.Inject
+
+class GetChatRoomByRidUseCase @Inject constructor(
+    private val repository: ChatRoomRepository
+) {
+    suspend operator fun invoke(
+        rid: String,
+        getChatRoomState: GetChatRoomState = GetChatRoomState.CheckAndGetDataFromLocal
+    ): GetChatRoomState {
+        return withContext(Dispatchers.IO) {
+            repository.getChatRoomWithRid(rid, getChatRoomState)
+        }
+    }
+}
