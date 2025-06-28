3d08f548c570e7f989d20c25ae7f163b484df1a7
ThinkGYLee <ntnpholicfp@gmail.com>
2025-06-29 02:41:00 +0900

    - GetChatRoomState.kt - Common 부분으로 일부 이동 - CreateGroupChat 관련 부분 추가 - ByRid 부분 명칭 수정

--- domain/src/main/java/com/gyleedev/domain/model/GetChatRoomState.kt
+++ domain/src/main/java/com/gyleedev/domain/model/GetChatRoomState.kt
@@ -5,25 +5,30 @@ sealed interface GetChatRoomState {
     // Common
     data object None : GetChatRoomState
     data object CheckAndGetDataFromLocal : GetChatRoomState
+    data object CreatingRemoteChatRoomData : GetChatRoomState
+    data object SavingGetChatRoomToLocal : GetChatRoomState
+    data object ReturnChatRoom : GetChatRoomState
 
     // ByUid
     data object CheckingRemoteGetChatRoomExists : GetChatRoomState
     data object GetRemoteData : GetChatRoomState
     data object CheckingMyDataExists : GetChatRoomState
     data object CheckingFriendDataExists : GetChatRoomState
-    data object CreatingRemoteChatRoomData : GetChatRoomState
     data object CheckingMyReceiverExists : GetChatRoomState
     data object CheckingFriendReceiverExists : GetChatRoomState
     data object InsertMyDataToReceiver : GetChatRoomState
     data object InsertFriendDataToReceiver : GetChatRoomState
     data object UpdateMyData : GetChatRoomState
     data object UpdateFriendData : GetChatRoomState
-    data object SavingGetChatRoomToLocal : GetChatRoomState
 
     // ByRid
-    data object GetRemoteReceivers : GetChatRoomState
-    data object CompareAndInsertReceiversToLocal : GetChatRoomState
-    data object ReturnChatRoom : GetChatRoomState
+    data object GetAndSyncReceivers : GetChatRoomState
+    data object InsertReceiversToLocal : GetChatRoomState
+
+    // CreateGroupChat
+    data object CreateRemoteGroupChatRoom : GetChatRoomState
+    data object InsertUserChatRoomsToRemote : GetChatRoomState
+    data object InsertReceiversToRemote : GetChatRoomState
 
     data class Success(
         val data: ChatRoomAndReceiverLocalData
