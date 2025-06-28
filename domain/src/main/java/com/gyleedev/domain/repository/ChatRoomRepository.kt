86c72582ee897c2784e4feb1210eb3d54602e44d
ThinkGYLee <ntnpholicfp@gmail.com>
2025-06-29 02:47:08 +0900

    - ChatRoomRepository.kt, ChatRoomRepositoryImpl.kt - 외부로 노출할 필요 없는 함수 private 으로 전환 - createMyUserChatRoom, createFriendUserChatRoom 에서 필요 없는 user 인수 제거 - insertChatRoomToLocal 에서 필요없는 user 인수 제거 - createGroupChat 구현 - insertUserChatRooms 구현 - insertReceiversToRemote 구현 - getChatRoomData 제거 - createUserChatRoom 추가 - getChatRoomIdFromRemote 제거 -

--- domain/src/main/java/com/gyleedev/domain/repository/ChatRoomRepository.kt
+++ domain/src/main/java/com/gyleedev/domain/repository/ChatRoomRepository.kt
@@ -2,9 +2,7 @@ package com.gyleedev.domain.repository
 
 import androidx.paging.PagingData
 import com.gyleedev.domain.model.ChatRoomAndReceiverLocalData
-import com.gyleedev.domain.model.ChatRoomData
 import com.gyleedev.domain.model.GetChatRoomState
-import com.gyleedev.domain.model.ProcessResult
 import com.gyleedev.domain.model.RelatedUserLocalData
 import kotlinx.coroutines.flow.Flow
 
@@ -24,32 +22,9 @@ interface ChatRoomRepository {
 
     suspend fun createGroupChat(
         users: List<RelatedUserLocalData>,
-        getChatRoomState: GetChatRoomState
+        getChatRoomState: GetChatRoomState = GetChatRoomState.CreateRemoteGroupChatRoom
     ): GetChatRoomState
 
-    fun checkChatRoomExistsInRemote(relatedUserLocalData: RelatedUserLocalData): Flow<Boolean>
-    suspend fun createChatRoomData(): Flow<ChatRoomData?>
-    fun createMyUserChatRoom(
-        relatedUserLocalData: RelatedUserLocalData,
-        chatRoomData: ChatRoomData
-    ): Flow<ProcessResult>
-
-    fun createFriendUserChatRoom(
-        relatedUserLocalData: RelatedUserLocalData,
-        chatRoomData: ChatRoomData
-    ): Flow<ProcessResult>
-
-    fun getChatRoomIdFromRemote(relatedUserLocalData: RelatedUserLocalData): Flow<String?>
-    fun getChatRoomFromRemote(relatedUserLocalData: RelatedUserLocalData): Flow<ChatRoomData?>
-    suspend fun insertChatRoomToLocal(
-        relatedUserLocalData: RelatedUserLocalData,
-        chatRoomData: ChatRoomData,
-        isGroup: Boolean = false
-    ): Long
-
-    suspend fun makeNewChatRoom(rid: String, receiver: String, isGroup: Boolean = false): Long
-
-    // suspend fun getChatRoomByUid(uid: String): ChatRoomLocalData?
     suspend fun resetChatRoomData()
     fun getChatRoomListWithPaging(): Flow<PagingData<ChatRoomAndReceiverLocalData>>
 }
