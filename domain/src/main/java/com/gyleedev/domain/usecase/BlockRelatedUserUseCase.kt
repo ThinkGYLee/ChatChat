7db5347f383d9a48f4029b7b14d96f362957fa03
ThinkGYLee <ntnpholicfp@gmail.com>
2025-06-29 23:57:52 +0900

    - BlockRelatedUserUseCase.kt - 리턴타입 명시되도록 수정

--- domain/src/main/java/com/gyleedev/domain/usecase/BlockRelatedUserUseCase.kt
+++ domain/src/main/java/com/gyleedev/domain/usecase/BlockRelatedUserUseCase.kt
@@ -1,5 +1,6 @@
 package com.gyleedev.domain.usecase
 
+import com.gyleedev.domain.model.ChangeRelationResult
 import com.gyleedev.domain.model.RelatedUserLocalData
 import com.gyleedev.domain.repository.UserRepository
 import javax.inject.Inject
@@ -7,6 +8,7 @@ import javax.inject.Inject
 class BlockRelatedUserUseCase @Inject constructor(
     private val repository: UserRepository
 ) {
-    suspend operator fun invoke(friend: RelatedUserLocalData) =
-        repository.blockRelatedUserRequest(friend)
+    suspend operator fun invoke(friend: RelatedUserLocalData): ChangeRelationResult {
+        return repository.blockRelatedUserRequest(friend)
+    }
 }
