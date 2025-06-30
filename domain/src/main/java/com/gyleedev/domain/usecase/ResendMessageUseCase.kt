cfef8e5df3bd5a909b15e41869caada5195a22bf
ThinkGYLee <ntnpholicfp@gmail.com>
2025-06-30 20:42:17 +0900

    - ResendMessageUseCase.kt - 테스트 플로우에 맞게 수정

--- domain/src/main/java/com/gyleedev/domain/usecase/ResendMessageUseCase.kt
+++ domain/src/main/java/com/gyleedev/domain/usecase/ResendMessageUseCase.kt
@@ -6,36 +6,46 @@ import com.gyleedev.domain.model.MessageData
 import com.gyleedev.domain.model.MessageSendState
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.first
-import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.withContext
 import java.time.Instant
 import javax.inject.Inject
 
+/**
+ *  1. 네트워크가 실패 했을 때
+ *  2. 네트워크가 성공 했을 때
+ *    2-2. 메시지 아이디가 있을 때
+ *      2-2.1. 메시지 보내고 성공한 경우 업데이트
+ *      2-2.2. 메시지 보내고 실패한 경우 업데이트가 아님
+ */
 class ResendMessageUseCase @Inject constructor(
     private val sendMessageToRemoteUseCase: SendMessageToRemoteUseCase,
     private val updateMessageStateUseCase: UpdateMessageStateUseCase,
     private val getMessageFromLocalUseCase: GetMessageFromLocalUseCase
 ) {
     @RequiresApi(Build.VERSION_CODES.O)
-    suspend operator fun invoke(messageData: MessageData, rid: Long, networkState: Boolean) {
+    suspend operator fun invoke(
+        messageData: MessageData,
+        rid: Long,
+        networkState: Boolean,
+        time: Long = Instant.now().toEpochMilli()
+    ) {
+        if (!networkState) {
+            return
+        }
         withContext(Dispatchers.IO) {
-            if (networkState) {
-                val messageId =
-                    getMessageFromLocalUseCase(messageData).firstOrNull()?.messageId
-                if (messageId != null) {
-                    val request = try {
-                        sendMessageToRemoteUseCase(messageData).first()
-                    } catch (e: Throwable) {
-                        println(e)
-                    }
-                    if (request is MessageSendState) {
-                        val message = messageData.copy(
-                            messageSendState = request,
-                            time = Instant.now().toEpochMilli()
-                        )
-                        updateMessageStateUseCase(messageId, rid, message)
-                    }
-                }
+            val messageId = getMessageFromLocalUseCase(messageData).first().messageId
+            val request = try {
+                sendMessageToRemoteUseCase(messageData).first()
+            } catch (e: Throwable) {
+                e.printStackTrace()
+                // TODO 처리
+            }
+            if (request is MessageSendState) {
+                val message = messageData.copy(
+                    messageSendState = request,
+                    time = time
+                )
+                updateMessageStateUseCase(messageId, rid, message)
             }
         }
     }
