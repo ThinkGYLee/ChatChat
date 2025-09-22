2d80a5570aecf5704e21d02e44ca1986567bca2b
ThinkGYLee <ntnpholicfp@gmail.com>
2025-09-22 23:43:42 +0900

    Test(Domain) : SignInAuthUseCaseTest.kt - 테스트케이스 분화

--- domain/src/test/java/com/gyleedev/domain/usecase/SignInAuthUseCaseTest.kt
+++ domain/src/test/java/com/gyleedev/domain/usecase/SignInAuthUseCaseTest.kt
@@ -6,11 +6,12 @@ import io.mockk.MockKAnnotations
 import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.impl.annotations.MockK
-import junit.framework.TestCase.assertEquals
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.Test
+import kotlin.test.assertEquals
 
 class SignInAuthUseCaseTest {
     private lateinit var useCase: SignInAuthUseCase
@@ -25,7 +26,7 @@ class SignInAuthUseCaseTest {
     }
 
     @Test
-    fun `회원 가입 인증`() {
+    fun `회원 가입 인증 성공 케이스`() {
         val id = "id"
         val password = "password"
         val nickname = "nickname"
@@ -38,9 +39,59 @@ class SignInAuthUseCaseTest {
         runTest {
             val answer = useCase.invoke(id, password, nickname)
 
-            assertEquals(result, answer)
+            assertEquals(result.first(), answer.first())
 
-            coVerify {
+            coVerify(exactly = 1) {
+                repository.signInUser(id, password, nickname)
+            }
+        }
+    }
+
+    @Test
+    fun `회원 가입 인증 실패 케이스`() {
+        val id = "id"
+        val password = "password"
+        val nickname = "nickname"
+        val result = flowOf(null)
+
+        coEvery {
+            repository.signInUser(id, password, nickname)
+        } returns result
+
+        runTest {
+            val answer = useCase.invoke(id, password, nickname)
+
+            assertEquals(result.first(), answer.first())
+
+            coVerify(exactly = 1) {
+                repository.signInUser(id, password, nickname)
+            }
+        }
+    }
+
+    @Test
+    fun `회원 가입 인증 예외 케이스`() {
+        val id = "id"
+        val password = "password"
+        val nickname = "nickname"
+        val error = RuntimeException("test")
+
+        coEvery {
+            repository.signInUser(id, password, nickname)
+        } throws error
+
+        runTest {
+            var thrown = false
+            try {
+                repository.signInUser(id, password, nickname)
+            } catch (e: Throwable) {
+                thrown = true
+                assertEquals(error.message, e.message)
+            }
+
+            assertEquals(true, thrown)
+
+            coVerify(exactly = 1) {
                 repository.signInUser(id, password, nickname)
             }
         }
