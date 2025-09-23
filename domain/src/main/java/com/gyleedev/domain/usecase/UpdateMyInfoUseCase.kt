40faab554982d3ed23eae748375a75122a0b3704
ThinkGYLee <ntnpholicfp@gmail.com>
2025-09-24 03:37:45 +0900

    Refactor(Domain) : UpdateMyInfoUseCase.kt - 리턴타입 수정

--- domain/src/main/java/com/gyleedev/domain/usecase/UpdateMyInfoUseCase.kt
+++ domain/src/main/java/com/gyleedev/domain/usecase/UpdateMyInfoUseCase.kt
@@ -3,14 +3,14 @@ package com.gyleedev.domain.usecase
 import com.gyleedev.domain.model.UserData
 import com.gyleedev.domain.repository.UserRepository
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class UpdateMyInfoUseCase @Inject constructor(
     private val repository: UserRepository,
 ) {
-    suspend operator fun invoke(user: UserData): Flow<Boolean> = withContext(Dispatchers.IO) {
-        repository.updateMyUserInfo(user)
+    suspend operator fun invoke(user: UserData): Boolean = withContext(Dispatchers.IO) {
+        repository.updateMyUserInfo(user).first()
     }
 }
