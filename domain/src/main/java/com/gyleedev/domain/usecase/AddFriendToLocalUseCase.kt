6e16e8307915284a7ce7d9a04bd166504a383b01
ThinkGYLee <ntnpholicfp@gmail.com>
2025-09-24 22:53:31 +0900

    Test(Domain) : Add AddFriendToLocalUseCaseTest - AddFriendToLocalUseCaseTest 구현 - AddFriendToLocalUseCase 리턴값 flow 해제

--- domain/src/main/java/com/gyleedev/domain/usecase/AddFriendToLocalUseCase.kt
+++ domain/src/main/java/com/gyleedev/domain/usecase/AddFriendToLocalUseCase.kt
@@ -3,10 +3,13 @@ package com.gyleedev.domain.usecase
 import com.gyleedev.domain.model.UserData
 import com.gyleedev.domain.repository.UserRepository
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
 import javax.inject.Inject
 
 class AddFriendToLocalUseCase @Inject constructor(
     private val repository: UserRepository,
 ) {
-    suspend operator fun invoke(userData: UserData): Flow<Boolean> = repository.insertFriendToLocal(userData)
+    suspend operator fun invoke(userData: UserData): Boolean {
+        return repository.insertFriendToLocal(userData).first()
+    }
 }
