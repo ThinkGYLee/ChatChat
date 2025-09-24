eb6cf64275f1870f06830ae07aad0144bac06b8c
ThinkGYLee <ntnpholicfp@gmail.com>
2025-09-25 00:08:22 +0900

    Style(Domain) : BlockUnknownUserUseCase.kt - 개행 수정 - 리턴타입 명시

--- domain/src/main/java/com/gyleedev/domain/usecase/BlockUnknownUserUseCase.kt
+++ domain/src/main/java/com/gyleedev/domain/usecase/BlockUnknownUserUseCase.kt
@@ -1,5 +1,6 @@
 package com.gyleedev.domain.usecase
 
+import com.gyleedev.domain.model.ChangeRelationResult
 import com.gyleedev.domain.model.UserData
 import com.gyleedev.domain.repository.UserRepository
 import javax.inject.Inject
@@ -7,5 +8,7 @@ import javax.inject.Inject
 class BlockUnknownUserUseCase @Inject constructor(
     private val repository: UserRepository,
 ) {
-    suspend operator fun invoke(userData: UserData) = repository.blockUnknownUserRequest(userData)
+    suspend operator fun invoke(userData: UserData): ChangeRelationResult {
+        return repository.blockUnknownUserRequest(userData)
+    }
 }
